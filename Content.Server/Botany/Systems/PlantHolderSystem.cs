cc498c0bbb83044b1a81d5eca4d1e53b1975e2d3
Pok245 <113675512+Pok27@users.noreply.github.com>
2025-08-02 21:32:13 +0300

    fix error 2

--- Content.Server/Botany/Systems/PlantHolderSystem.cs
+++ Content.Server/Botany/Systems/PlantHolderSystem.cs
@@ -538,7 +538,7 @@ public sealed partial class PlantHolderSystem : EntitySystem
                 return false;
             }
 
-            _botany.Harvest(component.Seed, user, component.YieldMod);
+            _botany.Harvest(component.Seed, user, plantholder);
             AfterHarvest(plantholder, component);
             return true;
         }
@@ -575,7 +575,7 @@ public sealed partial class PlantHolderSystem : EntitySystem
         if (component.Seed == null || !component.Harvest)
             return;
 
-        _botany.AutoHarvest(component.Seed, Transform(uid).Coordinates);
+        _botany.AutoHarvest(component.Seed, Transform(uid).Coordinates, uid);
         AfterHarvest(uid, component);
     }
 
