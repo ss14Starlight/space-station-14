5a8cf92bcd4602b3231e77d0fe23d26d91ca295d
walksanatora <walkerffo22@gmail.com>
2025-08-04 12:47:22 -0700

    scaffold the action a bit still gotta implement the action and the doafter but oh well.

---
+++ Content.Shared/_Starlight/Flockmind/FlockmindConverterComponent.cs
@@ -0,0 +1,8 @@
+namespace Content.Shared._Starlight.Flockmind;
+
+/// <summary>
+/// Marker component used by FlockmindConverterSystem to be able listen to actions.
+/// </summary>
+[RegisterComponent]
+public sealed partial class FlockmindConverterComponent : Component
+{ }
---
+++ Content.Shared/_Starlight/Flockmind/FlockmindConverterSystem.cs
@@ -0,0 +1,27 @@
+using Content.Shared.Actions;
+using Content.Shared.DoAfter;
+
+namespace Content.Shared._Starlight.Flockmind;
+
+public sealed partial class FlockmindConverterSystem : EntitySystem
+{
+    [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<FlockmindConverterComponent, FlockmindConvertTileEvent>(OnTileConvert);
+    }
+
+    private void OnTileConvert(Entity<FlockmindConverterComponent> ent, ref FlockmindConvertTileEvent ev)
+    {
+
+    }
+}
+
+
+public sealed partial class FlockmindConvertTileEvent : WorldTargetActionEvent
+{
+    [DataField]
+    public int ConvertCost = 15;
+}
