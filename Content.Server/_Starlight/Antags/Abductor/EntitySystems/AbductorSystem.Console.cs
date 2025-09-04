651df044c00cdf7a911b292891cce0a71bd84428
Nico L. <xxdarknight0xx@aol.com>
2025-09-04 15:39:26 -0700

    commented out for testing

--- Content.Server/_Starlight/Antags/Abductor/EntitySystems/AbductorSystem.Console.cs
+++ Content.Server/_Starlight/Antags/Abductor/EntitySystems/AbductorSystem.Console.cs
@@ -45,6 +45,11 @@ public sealed partial class AbductorSystem : SharedAbductorSystem
         args.Progress = AbductProgress(ent, _number.GetTarget(ent.Owner));
     }
 
+    private void ConsolePopup(EntityUid actor, string text)
+    {
+        _popup.PopupCursor(text, actor);
+    }
+
     private float AbductProgress(Entity<AbductConditionComponent> ent, int target)
     {
         AbductorScientistComponent? scientistComp = null;
@@ -161,6 +166,7 @@ public sealed partial class AbductorSystem : SharedAbductorSystem
         };
         _doAfter.TryStartDoAfter(doAfter);
     }
+
     private void OnDoAfterAttract(Entity<AbductorComponent> ent, ref AbductorAttractDoAfterEvent args)
     {
         if (args.Handled || args.Cancelled)
@@ -183,7 +189,7 @@ public sealed partial class AbductorSystem : SharedAbductorSystem
         {
             if (_mobState.IsIncapacitated(victim)) // If crit/dead don't teleport
             {
-                ConsolePopup(args.Actor, Loc.GetString("abductor-console-target-status-bad"));
+                // ConsolePopup(args.Actor, Loc.GetString("abductor-console-target-status-bad"));
                 return;
             }
             var dispenser = GetEntity(args.Dispencer);
