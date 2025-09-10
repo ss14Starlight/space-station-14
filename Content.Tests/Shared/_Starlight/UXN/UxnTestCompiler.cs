ca38e77b4273b6326c5c72a0cc740ea5a01be626
walksanatora <walkerffo22@gmail.com>
2025-09-10 08:38:13 -0700

    move compile test out of integration.

---
+++ Content.Tests/Shared/_Starlight/UXN/UxnTestCompiler.cs
@@ -0,0 +1,49 @@
+
+using System;
+using System.IO;
+using System.Reflection;
+using Content.Shared._Starlight.UXN;
+using Content.Shared._Starlight.UXN.Devices;
+using Content.Tests;
+using NUnit.Framework;
+using Robust.Shared.Utility;
+
+namespace Content.Tests.Shared._Starlight.UXN;
+
+[TestFixture]
+public sealed class UxnTestCompiler : ContentUnitTest
+{
+    [Test]
+    public void TestCompileOpctest()
+    {
+        var uxn = new UXNProcessor();
+        using Stream rom = Assembly.GetExecutingAssembly().GetManifestResourceStream("Content.Tests.drifloon.rom")!;
+
+        var mem = uxn.SystemMem;
+        var writeHead = 0x100;
+        Span<byte> span = new byte[32];
+        while (rom.CanRead)
+        {
+            var amnt = rom.Read(span);
+            if (amnt == 0) break;
+            for (int i = 0; i < amnt; i++)
+            {
+                mem[writeHead] = span[i];
+                writeHead++;
+            }
+        }
+
+        using Stream tal = Assembly.GetExecutingAssembly().GetManifestResourceStream("Content.Tests.opctest.tal")!;
+        using StreamReader talReader = new StreamReader(tal);
+        var uxnTal = talReader.ReadToEnd();
+
+        var stdio = uxn.AttachDevice(0x01, new FakeStdioDevice(uxnTal));
+
+        uxn.RunUnlimited();
+
+        Console.WriteLine($"Assembled UXN program in {uxn.InstructionCounter} instructions, FakedStdio provided {stdio.CharCount}/{uxnTal.Length} chars");
+
+        Assert.That(uxn.SystemDevice.Status, Is.GreaterThanOrEqualTo(0x80)); //since anything lesser is a error
+        Assert.That(stdio.FakedOutput.Count, Is.EqualTo(3373)); //and this is the expected rom size of compiled opctest
+    }
+}
