44fdab4cd5221d4684d46da4ef8e7840e9ebd29c
Arkanic <50847107+Arkanic@users.noreply.github.com>
2025-11-15 09:19:15 +1300

    credits deduct, need to test

---
+++ Content.Server/_Starlight/Devil/DamnationActions/DamnationActionDeductCredits.cs
@@ -0,0 +1,29 @@
+using Content.Shared._Starlight.Devil.Damnations;
+using Content.Shared.Starlight;
+using Content.Server.Administration.Managers;
+
+
+namespace Content.Server._Starlight.Devil.DamnationActions;
+
+public sealed partial class DamnationActionDeductCredits : DamnationAction
+{
+    [DataField]
+    int Amount = 5000;
+
+    private IPlayerRolesManager _playerRoles = default!;
+
+    public override bool Action(Entity<DamnedComponent> victim)
+    {
+        if (_playerRoles.GetPlayerData(victim) is not PlayerData playerData
+            || playerData.Balance < Amount
+            || Amount <= 0) return false;
+        
+        playerData.Balance -= Amount;
+        return true;
+    }
+
+    public override void ResolveIoC()
+    {
+        _playerRoles = IoCManager.Resolve<IPlayerRolesManager>();
+    }
+}
