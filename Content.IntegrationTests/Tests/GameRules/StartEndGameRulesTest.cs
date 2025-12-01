ef3dc1a7c910fad16d8601c95cf44d7eb1d630de
furrylover833 <82061820+furrylover833@users.noreply.github.com>
2025-12-01 10:56:25 +0100

    fixes

--- Content.IntegrationTests/Tests/GameRules/StartEndGameRulesTest.cs
+++ Content.IntegrationTests/Tests/GameRules/StartEndGameRulesTest.cs
@@ -48,6 +48,9 @@ public sealed class StartEndGameRulesTest
             Assert.That(!gameTicker.GetAddedGameRules().Any());
         });
 
+        // Give extra time for all cleanup operations to complete
+        await server.WaitRunTicks(5);
+
         await pair.CleanReturnAsync();
     }
 }
