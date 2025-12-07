823641f5319e8cadb195724f42ceee3cd6f46a16
mikeysaurus <23003816+mikeysaurus@users.noreply.github.com>
2025-12-07 00:42:44 -0800

    add crawling kinda

---
+++ Content.Shared/DeltaV/Abilities/AlwaysTriggerMousetrapComponent.cs
@@ -0,0 +1,6 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.DeltaV.Abilities;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class AlwaysTriggerMousetrapComponent : Component;
