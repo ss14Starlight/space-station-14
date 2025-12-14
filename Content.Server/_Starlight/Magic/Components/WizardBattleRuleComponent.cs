8bb4158c49c20d34818099799f2b6396af4aad88
Pablo Pardos <papardos@ucm.es>
2025-12-14 18:29:16 +0100

    Spawnpoints and shuttle
    Spawner and shuttles, also the base battle rule component
    
    things still to add
    - Add scarfs to the inventory of the archmage? Other option would be to add them to a locker in the shuttle or smth

---
+++ Content.Server/_Starlight/Magic/Components/WizardBattleRuleComponent.cs
@@ -0,0 +1,35 @@
+using Content.Server.GameTicking.Rules;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+using Robust.Shared.Maths;
+using System.Numerics;
+
+namespace Content.Server._Starlight.Magic.Components;
+
+/// <summary>
+/// Component for the Wizard Battle game rule that loads two wizard shuttles.
+/// </summary>
+[RegisterComponent]
+public sealed partial class WizardBattleRuleComponent : Component
+{
+    /// <summary>
+    /// Path to the wizards shuttle grids.
+    /// </summary>
+    [DataField]
+    public ResPath ShuttlePathBlue = new("/Maps/_Starlight/ArchMageShuttleBlue.yml");
+
+    [DataField]
+    public ResPath ShuttlePathRed = new("/Maps/_Starlight/ArchMageShuttleRed.yml");
+
+    /// <summary>
+    /// Offset for the red faction shuttle.
+    /// </summary>
+    [DataField]
+    public Vector2 RedOffset = new(-1000, 0);
+
+    /// <summary>
+    /// Offset for the blue faction shuttle.
+    /// </summary>
+    [DataField]
+    public Vector2 BlueOffset = new(1000, 0);
+}
