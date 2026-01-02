b61e454273099b714b153aa0191125edb3ba4d4e
mikey <23003816+mikeysaurus@users.noreply.github.com>
2026-01-01 18:17:34 -0800

    rename, match syntax

--- Content.Shared/_Starlight/CCVar/StarlightCCVar.Shuttle.cs
+++ Content.Shared/_Starlight/CCVar/StarlightCCVar.Shuttle.cs
@@ -10,5 +10,5 @@ public sealed partial class StarlightCCVars
     ///     accordingly (like ArrivalsCooldown) in CCVars.Shuttle.cs.
     /// </summary>
     public static readonly CVarDef<float> ArrivalsFTLCooldown =
-        CVarDef.Create("arrivals.ftl.cooldown", 10f, CVar.SERVERONLY);
+        CVarDef.Create("arrivals.cooldown", 10f, CVar.SERVERONLY);
 }
