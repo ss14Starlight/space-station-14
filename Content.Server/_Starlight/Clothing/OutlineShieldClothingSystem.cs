b4cf780def164e403825a123d11e223744e2eff4
furrylover833 <82061820+furrylover833@users.noreply.github.com>
2026-01-07 17:11:16 +0100

    patch

--- Content.Server/_Starlight/Clothing/OutlineShieldClothingSystem.cs
+++
@@ -1,7 +0,0 @@
-using Content.Shared._Starlight.Clothing;
-
-namespace Content.Server._Starlight.Clothing;
-
-public sealed class OutlineShieldClothingSystem : SharedOutlineShieldClothingSystem
-{
-}
