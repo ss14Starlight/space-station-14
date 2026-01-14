e4ddfc3a9cb80308368d49e19a28f4e850bfd46d
CrazyPhantom779 <izayussedwards@gmail.com>
2026-01-14 15:52:02 +1100

    Trigger fix for predicted

--- Content.Server/_Starlight/Trigger/Systems/TriggerOnBatteryFullSystem.cs
+++ Content.Server/_Starlight/Trigger/Systems/TriggerOnBatteryFullSystem.cs
@@ -1,9 +1,9 @@
 using Content.Server._Starlight.Trigger.Components.Triggers;
-using Content.Server.Power.Components;
 using Content.Server.Power.EntitySystems;
 using Content.Shared.Trigger.Systems;
 using Content.Shared.Power;
 using Content.Shared.Power.Components;
+using Content.Shared.Power.EntitySystems;
 
 namespace Content.Server._Starlight.Trigger.Systems;
 
@@ -11,17 +11,27 @@ public sealed class TriggerOnBatteryFullSystem : EntitySystem
 {
     [Dependency] private readonly TriggerSystem _trigger = default!;
     [Dependency] private readonly BatterySystem _battery = default!;
+    [Dependency] private readonly PredictedBatterySystem _predictedBattery = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<TriggerOnBatteryFullComponent, ChargeChangedEvent>(OnChargeChanged);
+        SubscribeLocalEvent<TriggerOnBatteryFullComponent, PredictedBatteryChargeChangedEvent>(OnPredictedChargeChanged);
     }
 
     private void OnChargeChanged(Entity<TriggerOnBatteryFullComponent> ent, ref ChargeChangedEvent args)
     {
         if (TryComp(ent.Owner, out BatteryComponent? battery) && _battery.IsFull((ent.Owner, battery)))
+        {
+            _trigger.Trigger(ent);
+        }
+    }
+
+    private void OnPredictedChargeChanged(Entity<TriggerOnBatteryFullComponent> ent, ref PredictedBatteryChargeChangedEvent args)
+    {
+        if (TryComp(ent.Owner, out PredictedBatteryComponent? battery) && _predictedBattery.IsFull((ent.Owner, battery)))
         {
             _trigger.Trigger(ent);
         }
