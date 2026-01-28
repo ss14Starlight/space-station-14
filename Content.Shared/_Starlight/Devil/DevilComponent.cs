4058e3383e55986bc40aa07dd4a966e57110f327
Arkanic <50847107+Arkanic@users.noreply.github.com>
2026-01-28 18:46:26 +1300

    switch to rads, i swear this has a purpose

--- Content.Shared/_Starlight/Devil/DevilComponent.cs
+++ Content.Shared/_Starlight/Devil/DevilComponent.cs
@@ -111,7 +111,7 @@ public sealed partial class DevilComponent : Component
     {
         DamageDict = new()
         {
-            { "Cellular", 10 },
+            { "Radiation", 10 },
         }
     };
 
