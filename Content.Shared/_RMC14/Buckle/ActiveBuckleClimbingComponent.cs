d13231997891a7cf44dcdb828191956300bbb5a7
Conflee <46701008+Conflee@users.noreply.github.com>
2026-02-10 19:02:38 -0500

    broken shit

---
+++ Content.Shared/_RMC14/Buckle/ActiveBuckleClimbingComponent.cs
@@ -0,0 +1,11 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Buckle;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(RMCBuckleSystem))]
+public sealed partial class ActiveBuckleClimbingComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public EntityUid? Strap;
+}
