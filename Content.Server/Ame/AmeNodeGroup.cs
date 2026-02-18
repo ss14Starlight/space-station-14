59d7f291bc6dfaa90819b06ed1e44145c2dddf82
nkokic <nikola1.kokic@gmail.com>
2026-02-18 17:34:09 +0100

    linearized AME power

--- Content.Server/Ame/AmeNodeGroup.cs
+++ Content.Server/Ame/AmeNodeGroup.cs
@@ -183,20 +183,28 @@ public sealed class AmeNodeGroup : BaseNodeGroup
     }
     /* End STARLIGHT Control of AME Injection */
 
+    /* Start STARLIGHT Linearization of AME power */
     /// <summary>
     /// Calculates the amount of power the AME can produce with the given settings
     /// </summary>
     public float CalculatePower(int fuel, int cores)
     {
-        // Balanced around a single core AME with injection level 2 producing 120KW.
-        // Two core with four injection is 150kW. Two core with two injection is 90kW.
+        // parametrized linear AME function graph at https://www.desmos.com/calculator/r523dxiqna
+        float wattsPerCore = 80000f;
+        float startEfficency = 0.5f;
+        float tailPenaltyFactor = 0.9f;
+        
+        float efficency = startEfficency * fuel / (2 * cores);
+        
+        if (fuel >= 2 * cores - 1)
+            efficency += (fuel - (2 * cores - 1)) * (1 - startEfficency);
 
-        // Increasing core count creates diminishing returns, increasing injection amount increases 
-        // Unlike the previous solution, increasing fuel and cores always leads to an increase in power, even if by very small amounts.
-        // Increasing core count without increasing fuel always leads to reduced power as well.
-        // At 18+ cores and 2 inject, the power produced is less than 0, the Max ensures the AME can never produce "negative" power.
-        return MathF.Max(200000f * MathF.Log10(2 * fuel * MathF.Pow(cores, (float)-0.5)), 0);
+        if (fuel >= 2 * cores)
+            efficency -= (fuel - (2 * cores)) * (1 - startEfficency) * tailPenaltyFactor;
+
+        return efficency * wattsPerCore * cores;
     }
+    /* End STARLIGHT Linearization of AME power */
 
     public int GetTotalStability()
     {
