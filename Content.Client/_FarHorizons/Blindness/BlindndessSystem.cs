cae192fad1b1a7bc8c1a6fc5c212f3fae4c6fdca
FoxxoTrystan <trystan.garnierhein@gmail.com>
2026-03-20 18:58:16 +0100

    Surgeries

--- Content.Client/_FarHorizons/Blindness/BlindndessSystem.cs
+++
@@ -1,5 +0,0 @@
-using Content.Shared._FarHorizons.StatusEffects.Blindness;
-
-namespace Content.Client._FarHorizons.StatusEffects.Blindness;
-
-public sealed partial class BlindnessSystem : SharedBlindnessSystem;
--- Content.Server/_FarHorizons/StatusEffects/Blindness/BlindndessSystem.cs
+++
@@ -1,5 +0,0 @@
-using Content.Shared._FarHorizons.StatusEffects.Blindness;
-
-namespace Content.Server._FarHorizons.StatusEffects.Blindness;
-
-public sealed partial class BlindnessSystem : SharedBlindnessSystem;
