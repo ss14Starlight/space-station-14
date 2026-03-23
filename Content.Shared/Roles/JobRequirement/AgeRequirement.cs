b8d409c3d335657fd56760ba185a1809dc20c102
Red Mushie <noreply@redmushie.me>
2026-03-23 13:06:35 +0100

    Comments in AgeRequiment

--- Content.Shared/Roles/JobRequirement/AgeRequirement.cs
+++ Content.Shared/Roles/JobRequirement/AgeRequirement.cs
@@ -23,34 +23,36 @@ public sealed partial class AgeRequirement : JobRequirement
         IPrototypeManager protoManager,
         HumanoidCharacterProfile? profile,
         IReadOnlyDictionary<string, TimeSpan>? playTimes,
-        out FormattedMessage reason)
+        out FormattedMessage reason) // Starlight: Always return requirement description
     {
         reason = new FormattedMessage();
 
         if (profile is null) //the profile could be null if the player is a ghost. In this case we don't need to block the role selection for ghostrole
             return true;
 
+        // Starlight BEGIN: Reason is successful by default
         reason = FormattedMessage.FromMarkupPermissive(Loc.GetString(
             Inverted ? "role-timer-age-young-enough" : "role-timer-age-old-enough",
             ("age", RequiredAge)));
+        // Starlight END
 
         if (!Inverted)
         {
-            if (profile.Age >= RequiredAge)
+            if (profile.Age >= RequiredAge) // Starlight BEGIN
                 return true;
 
             reason = FormattedMessage.FromMarkupPermissive(Loc.GetString("role-timer-age-not-old-enough",
                 ("age", RequiredAge)));
-            return false;
+            return false; // Starlight END
         }
         else
         {
-            if (profile.Age <= RequiredAge)
+            if (profile.Age <= RequiredAge) // Starlight BEGIN
                 return true;
 
             reason = FormattedMessage.FromMarkupPermissive(Loc.GetString("role-timer-age-not-young-enough",
                 ("age", RequiredAge)));
-            return false;
+            return false; // Starlight END
         }
     }
 }
