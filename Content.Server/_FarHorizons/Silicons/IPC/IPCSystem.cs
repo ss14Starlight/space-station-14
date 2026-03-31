6e97b077f1b104d7b8c9f1ace4301f2c81a1217b
FoxxoTrystan <trystan.garnierhein@gmail.com>
2026-03-31 20:30:31 +0200

    Fix Bugs related to Power

--- Content.Server/_FarHorizons/Silicons/IPC/IPCSystem.cs
+++ Content.Server/_FarHorizons/Silicons/IPC/IPCSystem.cs
@@ -24,6 +24,7 @@ using Robust.Shared.Timing;
 using Content.Shared.Damage.Systems;
 using Content.Shared.Power.EntitySystems;
 using Content.Shared.Inventory;
+using Content.Server.Electrocution;
 
 namespace Content.Server._FarHorizons.Silicons.IPC;
 
@@ -56,6 +57,7 @@ public sealed partial class IPCSystem : SharedIPCSystem
     [Dependency] private readonly TagSystem _tag = default!;
     [Dependency] private readonly BloodstreamSystem _bloodstream = default!;
     [Dependency] private readonly InventorySystem _inventorySystem = default!;
+    [Dependency] private readonly ElectrocutionSystem _electrocution = default!;
 
 
     public override void Initialize()
