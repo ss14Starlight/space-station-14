61bbcc2cacb9b84332237a3039849b429227b6c5
mikeysaurus <23003816+mikeysaurus@users.noreply.github.com>
2026-04-03 02:05:43 -0700

    Predict crawl under objects, also fix mispredict lag.

--- Content.Shared/_DeltaV/Abilities/CrawlUnderObjectsComponent.cs
+++ Content.Shared/_DeltaV/Abilities/CrawlUnderObjectsComponent.cs
@@ -1,5 +1,4 @@
 using Content.Shared.Actions;
-using DrawDepth = Content.Shared.DrawDepth.DrawDepth;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
@@ -9,13 +8,7 @@ namespace Content.Shared._DeltaV.Abilities;
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class CrawlUnderObjectsComponent : Component
 {
-    [DataField]
-    public EntityUid? ToggleHideAction;
-
-    [DataField]
-    public EntProtoId? ActionProto;
-
-    [DataField]
+    [DataField, AutoNetworkedField]
     public bool Enabled = false;
 
     /// <summary>
@@ -26,10 +19,22 @@ public sealed partial class CrawlUnderObjectsComponent : Component
     public List<(string key, int originalMask)> ChangedFixtures = new();
 
     [DataField]
+    public float SneakSpeedModifier = 0.5f;
+
+    /// <summary>
+    ///     CLIENT ONLY variable to save the draw old draw depth of the entity before it started crawling.
+    /// </summary>
+    [DataField]
     public int? OriginalDrawDepth;
 
+    /// <summary>
+    ///     Reference to the action itself.
+    /// </summary>
     [DataField]
-    public float SneakSpeedModifier = 0.5f;
+    public EntityUid? ToggleHideAction;
+
+    [DataField]
+    public EntProtoId? ActionProto;
 }
 
 [Serializable, NetSerializable]
@@ -39,9 +44,3 @@ public enum SneakMode : byte
 }
 
 public sealed partial class ToggleCrawlingStateEvent : InstantActionEvent { }
-
-[Serializable, NetSerializable]
-public sealed partial class CrawlingUpdatedEvent(bool enabled = false) : EventArgs
-{
-    public readonly bool Enabled = enabled;
-}
--- Resources/Locale/en-US/_DeltaV/actions/crawl-under-objects.ftl
+++ Resources/Locale/en-US/_DeltaV/actions/crawl-under-objects.ftl
@@ -1,2 +1,3 @@
 crawl-under-objects-toggle-on = Now sneaking
 crawl-under-objects-toggle-off = Now standing
+crawl-under-objects-toggle-off-fail = You can't stand up here!
