4a244c913621245ee49264ebe16bae48bbf64b96
neomoth <69372252+neomoth@users.noreply.github.com>
2026-04-06 09:40:51 -0400

    remove pokerchip stuff, moving to separate PR

--- Content.Server/_Starlight/Economy/PokerChips/PokerChipSystem.cs
+++
@@ -1,5 +0,0 @@
-using Content.Shared._Starlight.Economy.PokerChips.Systems;
-
-namespace Content.Server._Starlight.Economy.PokerChips;
-
-public sealed class PokerChipSystem : SharedPokerChipSystem;
