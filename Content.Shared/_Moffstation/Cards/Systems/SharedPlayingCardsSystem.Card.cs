44f5b742e3c8c11d3bf57095888334f9b8ef0ff0
Huaqas <zacswebb@gmail.com>
2026-04-08 00:33:46 +0800

    Playing Cards LOC Patch (#1210)
    Update SharedPlayingCardsSystem.Card.cs

--- Content.Shared/_Moffstation/Cards/Systems/SharedPlayingCardsSystem.Card.cs
+++ Content.Shared/_Moffstation/Cards/Systems/SharedPlayingCardsSystem.Card.cs
@@ -306,13 +306,15 @@ public abstract partial class SharedPlayingCardsSystem
         comp.ReverseLayers = deck.CommonReverseLayers.WithUnlessAlreadySpecified(rsiPath: deck.RsiPath.ToString());
         comp.FaceDown = data.Card.FaceDown;
 
-        (string, object)[] locArgs = suit is null
-            ? [("card", Loc.GetString(deck.CardValueLoc, ("card", data.Card.Id.ToLowerInvariant())))]
-            :
-            [
-                ("suit", Loc.GetString(deck.SuitLoc, ("suit", suit.ID.ToLowerInvariant()))),
-                ("card", Loc.GetString(deck.CardValueLoc, ("card", data.Card.Id.ToLowerInvariant()))),
-            ];
+        var idAndCard = new List<(string, object)>
+        {
+        ("id", data.Card.Id.ToLowerInvariant()),
+        ("card", Loc.GetString(deck.CardValueLoc, ("card", data.Card.Id.ToLowerInvariant()))),
+        };
+        var idCardAndSuit = suit is null
+        ? idAndCard
+        : idAndCard.Concat([("suit", Loc.GetString(deck.SuitLoc, ("suit", suit.ID.ToLowerInvariant())))]);
+        var locArgs = idCardAndSuit.ToArray();
 
         comp.ObverseName = Loc.GetString(data.Card.NameLoc ?? deck.CardNameLoc, locArgs);
         comp.Description = Loc.GetString(deck.CardDescLoc, locArgs);
