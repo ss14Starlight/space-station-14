b981db352a240493a03eb06861cd9d505ff9b789
neomoth <69372252+neomoth@users.noreply.github.com>
2026-04-10 11:38:36 -0400

    move new component fields to bottom of component

--- Content.Shared/Ghost/GhostComponent.cs
+++ Content.Shared/Ghost/GhostComponent.cs
@@ -43,19 +43,6 @@ public sealed partial class GhostComponent : Component
     [DataField, AutoNetworkedField]
     public EntityUid? BooActionEntity;
 
-    //Starlight begin
-    /// <summary>
-    /// Permits this ghost to speak in local chat instead of forwarding local messages to dead ooc.
-    /// </summary>
-    [DataField] public bool BypassGhostChat;
-
-    /// <summary>
-    /// Ensures this ghost always remains visible, this means both to alive players, and preventing clientside toggling.
-    /// This is read-only since you should be using the corporeal command for this.
-    /// </summary>
-    [DataField, AutoNetworkedField, ViewVariables] public bool AlwaysVisible;
-    //Starlight end
-
     // End actions
 
     /// <summary>
@@ -106,6 +93,19 @@ public sealed partial class GhostComponent : Component
     /// <remarks>Used to allow admins to change ghost colors. Should be removed if the capability to edit existing sprite colors is ever added back.</remarks>
     [DataField, AutoNetworkedField]
     public Color Color = Color.White;
+
+    //Starlight begin
+    /// <summary>
+    /// Permits this ghost to speak in local chat instead of forwarding local messages to dead ooc.
+    /// </summary>
+    [DataField] public bool BypassGhostChat;
+
+    /// <summary>
+    /// Ensures this ghost always remains visible, this means both to alive players, and preventing clientside toggling.
+    /// This is read-only since you should be using the corporeal command for this.
+    /// </summary>
+    [DataField, AutoNetworkedField, ViewVariables] public bool AlwaysVisible;
+    //Starlight end
 }
 
 public sealed partial class ToggleFoVActionEvent : InstantActionEvent { }
