0ea20999de1146b70298ba4a64b9ea2688f60c16
JMxm5 <274015191+JMxm5@users.noreply.github.com>
2026-04-11 11:05:25 +0200

    Deduplicate update code and only send updates when the state changes

--- Content.Client/Atmos/Monitor/UI/Widgets/ThresholdControl.xaml.cs
+++ Content.Client/Atmos/Monitor/UI/Widgets/ThresholdControl.xaml.cs
@@ -105,8 +105,9 @@ public sealed partial class ThresholdControl : BoxContainer
 
         _enabled.OnToggled += args =>
         {
-            _threshold.Ignore = !args.Pressed;
-            ThresholdDataChanged!.Invoke(_type, _threshold, _gas);
+            // Starlight-start: Air alarm buttons for enabling/disabling all thresholds
+            SetEnabled(args.Pressed);
+            // Starlight-end
         };
         _enabled.Pressed = !_threshold.Ignore;
     }
@@ -167,6 +168,9 @@ public sealed partial class ThresholdControl : BoxContainer
     // Starlight-start: Air alarm buttons for enabling/disabling all thresholds
     public void SetEnabled(bool enabled)
     {
+        if (_threshold.Ignore == !enabled)
+            return;
+
         _enabled.Pressed = enabled;
         _threshold.Ignore = !enabled;
         ThresholdDataChanged!.Invoke(_type, _threshold, _gas);
