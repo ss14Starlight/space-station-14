30bba6d535dc91f4b5d426231f6dad2b3b1566a9
Rinary <rinary.super@gmail.com>
2026-04-13 21:38:55 +0300

    fix name violations

--- Content.Server/_Starlight/Administration/Systems/Commands/KillSignCommand.cs
+++ Content.Server/_Starlight/Administration/Systems/Commands/KillSignCommand.cs
@@ -11,31 +11,31 @@ namespace Content.Server._Starlight.Administration.Systems.Commands;
 [AdminCommand(AdminFlags.Fun)]
 public sealed class KillSignCommand : ToolshedCommand
 {
-    private static readonly string BaseContentPath = "Objects/Misc/killsign.rsi";
-    private static readonly string SLContentPath = "_Starlight/Objects/Misc/killsign.rsi";
+    private static readonly string _baseContentPath = "Objects/Misc/killsign.rsi";
+    private static readonly string _sLContentPath = "_Starlight/Objects/Misc/killsign.rsi";
 
-    private static readonly Dictionary<string, (string path, string state)> Sprites = new()
+    private static readonly Dictionary<string, (string path, string state)> _sprites = new()
     {
-        ["kill"] = (BaseContentPath, "kill"),
-        ["raider"] = (BaseContentPath, "raider"),
-        ["peak"] = (BaseContentPath, "peak"),
-        ["nerd"] = (BaseContentPath, "nerd"),
-        ["it"] = (BaseContentPath, "it"),
-        ["furry"] = (BaseContentPath, "furry"),
-        ["dog"] = (BaseContentPath, "dog"),
-        ["cat"] = (BaseContentPath, "cat"),
-        ["bald"] = (BaseContentPath, "bald"),
-        ["w"] = (SLContentPath, "w"),
-        ["vip"] = (SLContentPath, "vip"),
-        ["ssd"] = (SLContentPath, "ssd"),
-        ["uwu"] = (SLContentPath, "uwu"),
-        ["owo"] = (SLContentPath, "owo"),
-        ["l"] = (SLContentPath, "l"),
-        ["honk"] = (SLContentPath, "honk"),
-        ["event"] = (SLContentPath, "event"),
-        ["dm"] = (SLContentPath, "dm"),
-        ["clueless"] = (SLContentPath, "clueless"),
-        ["admin"] = (SLContentPath, "admin"),
+        ["kill"] = (_baseContentPath, "kill"),
+        ["raider"] = (_baseContentPath, "raider"),
+        ["peak"] = (_baseContentPath, "peak"),
+        ["nerd"] = (_baseContentPath, "nerd"),
+        ["it"] = (_baseContentPath, "it"),
+        ["furry"] = (_baseContentPath, "furry"),
+        ["dog"] = (_baseContentPath, "dog"),
+        ["cat"] = (_baseContentPath, "cat"),
+        ["bald"] = (_baseContentPath, "bald"),
+        ["w"] = (_sLContentPath, "w"),
+        ["vip"] = (_sLContentPath, "vip"),
+        ["ssd"] = (_sLContentPath, "ssd"),
+        ["uwu"] = (_sLContentPath, "uwu"),
+        ["owo"] = (_sLContentPath, "owo"),
+        ["l"] = (_sLContentPath, "l"),
+        ["honk"] = (_sLContentPath, "honk"),
+        ["event"] = (_sLContentPath, "event"),
+        ["dm"] = (_sLContentPath, "dm"),
+        ["clueless"] = (_sLContentPath, "clueless"),
+        ["admin"] = (_sLContentPath, "admin"),
     };
 
     private EntityUid ApplyKillSign(EntityUid uid, (string path, string state) data)
@@ -49,7 +49,7 @@ public sealed class KillSignCommand : ToolshedCommand
     [CommandImplementation("set")]
     public EntityUid Set(IInvocationContext ctx, [PipedArgument] EntityUid uid, string type)
     {
-        if (!Sprites.TryGetValue(type, out var data))
+        if (!_sprites.TryGetValue(type, out var data))
         {
             ctx.WriteLine($"Unknown kill sign type: {type}");
             return uid;
@@ -68,7 +68,7 @@ public sealed class KillSignCommand : ToolshedCommand
     [CommandImplementation("set")]
     public IEnumerable<EntityUid> Set(IInvocationContext ctx, [PipedArgument] IEnumerable<EntityUid> uid, string type)
     {
-        if (!Sprites.TryGetValue(type, out var data))
+        if (!_sprites.TryGetValue(type, out var data))
         {
             ctx.WriteLine($"Unknown kill sign type: {type}");
             return uid;
