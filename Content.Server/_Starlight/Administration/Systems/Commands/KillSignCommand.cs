ecfde0283f52f68f2326d0867d89ca4bd1428586
Rinary <rinary.super@gmail.com>
2026-04-13 21:42:16 +0300

    doc strings

--- Content.Server/_Starlight/Administration/Systems/Commands/KillSignCommand.cs
+++ Content.Server/_Starlight/Administration/Systems/Commands/KillSignCommand.cs
@@ -38,6 +38,12 @@ public sealed class KillSignCommand : ToolshedCommand
         ["admin"] = (_sLContentPath, "admin"),
     };
 
+    /// <summary>
+    /// Applies a kill sign to the specified entity UID with the given sprite data.
+    /// </summary>
+    /// <param name="uid">Target entity UID</param>
+    /// <param name="data">Sprite data tuple containing path and state</param>
+    /// <returns>The UID of the entity with the applied kill sign</returns>
     private EntityUid ApplyKillSign(EntityUid uid, (string path, string state) data)
     {
         var comp = EnsureComp<KillSignComponent>(uid);
@@ -46,6 +52,13 @@ public sealed class KillSignCommand : ToolshedCommand
         return uid;
     }
 
+    /// <summary>
+    /// Command which applies a kill sign to the specified entity UID based on the provided type. If the type is unknown, it will notify the user.
+    /// </summary>
+    /// <param name="ctx">Invocation context</param>
+    /// <param name="uid">Target entity UID</param>
+    /// <param name="type">Kill sign type</param>
+    /// <returns>The UID of the entity with the applied kill sign</returns>
     [CommandImplementation("set")]
     public EntityUid Set(IInvocationContext ctx, [PipedArgument] EntityUid uid, string type)
     {
@@ -58,6 +71,11 @@ public sealed class KillSignCommand : ToolshedCommand
         return ApplyKillSign(uid, data);
     }
 
+    /// <summary>
+    /// Command which removes a kill sign on the specified entity UID by removing the KillSignComponent. If the entity does not have a kill sign, this command will have no effect.
+    /// </summary>
+    /// <param name="uid">Target entity UID</param>
+    /// <returns>The UID of the entity with the removed kill sign</returns>
     [CommandImplementation("rm")]
     public EntityUid RemoveKillSign([PipedArgument] EntityUid uid)
     {
@@ -65,6 +83,13 @@ public sealed class KillSignCommand : ToolshedCommand
         return uid;
     }
 
+    /// <summary>
+    /// List command variation of <seealso cref="Set(IInvocationContext, EntityUid, string)"/>
+    /// </summary>
+    /// <param name="ctx">Invocation context</param>
+    /// <param name="uid">Target entity UIDs</param>
+    /// <param name="type">Kill sign type</param>
+    /// <returns>The UIDs of the entities with the applied kill signs</returns>
     [CommandImplementation("set")]
     public IEnumerable<EntityUid> Set(IInvocationContext ctx, [PipedArgument] IEnumerable<EntityUid> uid, string type)
     {
@@ -77,6 +102,11 @@ public sealed class KillSignCommand : ToolshedCommand
         return uid.Select(x => ApplyKillSign(x, data));
     }
 
+    /// <summary>
+    /// List command variation of <seealso cref="RemoveKillSign(EntityUid)"/>
+    /// </summary>
+    /// <param name="uid">Target entity UIDs</param>
+    /// <returns>The UIDs of the entities with the removed kill signs</returns>
     [CommandImplementation("rm")]
     public IEnumerable<EntityUid> RemoveKillSign([PipedArgument] IEnumerable<EntityUid> uid)
         => uid.Select(RemoveKillSign);
