5dde95226d6b9ffd3fb232e7da73a13994972d07
neomoth <69372252+neomoth@users.noreply.github.com>
2026-04-16 15:03:49 -0400

    remove more assets

--- Content.Shared/_Starlight/SSDIndicator/AdminSSDIndicatorSystem.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Shared._Starlight.SSDIndicator;
-
-/// <summary>
-/// System for showing a red SSD indicator if a player is being actively messaged by an admin.
-/// </summary>
-public sealed class AdminSSDIndicatorSystem : EntitySystem
-{
-
-}
