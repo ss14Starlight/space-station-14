f3c9b8b45aa6e8c1d58a57c5e595e7ca7062116c
Red Mushie <noreply@redmushie.me>
2026-04-16 14:34:07 +0200

    Upgrade EORG prevention to its own system, move lots to shared and make many more things prevented

---
+++ Content.Client/_Starlight/EndOfRoundGriefing/PreventEorgSystem.cs
@@ -0,0 +1,22 @@
+﻿using Content.Shared._Starlight.EndOfRoundGriefing;
+
+namespace Content.Client._Starlight.EndOfRoundGriefing;
+
+/// <summary>
+/// <inheritdoc/>
+/// </summary>
+public sealed class PreventEorgSystem : SharedPreventEorgSystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeNetworkEvent<PreventEorgStateEvent>(OnStateChanged); // Handle server state changes.
+        RaiseNetworkEvent(new RequestPreventEorgStateEvent()); // Ask server to send a state.
+    }
+
+    private void OnStateChanged(PreventEorgStateEvent ev)
+    {
+        IsEnabled = ev.IsEnabled;
+        HasRoundEnded = ev.IsRoundEnded;
+    }
+}
---
+++ Content.Shared/_Starlight/EndOfRoundGriefing/SharedPreventEorgSystem.cs
@@ -0,0 +1,107 @@
+﻿using Content.Shared._Starlight.EndOfRoundGriefing.Components;
+using Content.Shared.Actions.Events;
+using Content.Shared.Interaction;
+using Content.Shared.Interaction.Events;
+using Content.Shared.Popups;
+using Content.Shared.Sticky;
+using Content.Shared.Trigger;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._Starlight.EndOfRoundGriefing;
+
+/// <summary>
+/// Intercepts various events and checks if they can be done.
+/// </summary>
+public abstract class SharedPreventEorgSystem : EntitySystem
+{
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+
+    protected bool IsEnabled = false;
+    protected bool HasRoundEnded = false;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<EorgPreventActionComponent, ActionValidateEvent>(OnValidateAction);
+        SubscribeLocalEvent<EorgPreventUseItemComponent, UseAttemptEvent>(OnUseAttempt);
+        SubscribeLocalEvent<EorgPreventTriggerComponent, AttemptTriggerEvent>(OnAttemptTrigger);
+        SubscribeLocalEvent<EorgPreventHandInteractComponent, BeforeInteractHandEvent>(OnBeforeInteractHand);
+        SubscribeLocalEvent<EorgPreventRangedInteractComponent, BeforeRangedInteractEvent>(OnBeforeRangedInteract);
+        SubscribeLocalEvent<EorgPreventStickingComponent, AttemptEntityStickEvent>(OnAttemptStick);
+    }
+
+    private void OnAttemptStick(EntityUid uid, EorgPreventStickingComponent component, ref AttemptEntityStickEvent args)
+    {
+        if (!IsEnabled || !HasRoundEnded) return;
+        if (args.Cancelled) return;
+        if (!HasComp<PreventEorgComponent>(args.User)) return;
+
+        _popup.PopupPredicted(Loc.GetString("eorg-action"), args.User, args.User, PopupType.LargeCaution);
+        args.Cancelled = true;
+    }
+
+    private void OnUseAttempt(EntityUid uid, EorgPreventUseItemComponent component, ref UseAttemptEvent args)
+    {
+        if (!IsEnabled || !HasRoundEnded) return;
+        if (args.Cancelled) return;
+        if (!HasComp<PreventEorgComponent>(uid)) return;
+
+        _popup.PopupPredicted(Loc.GetString("eorg-action"), uid, uid, PopupType.LargeCaution);
+        args.Cancel();
+    }
+
+    private void OnAttemptTrigger(EntityUid uid, EorgPreventTriggerComponent component, ref AttemptTriggerEvent args)
+    {
+        if (!IsEnabled || !HasRoundEnded) return;
+        if (!args.User.HasValue || args.Cancelled) return;
+        if (!HasComp<PreventEorgComponent>(args.User)) return;
+
+        _popup.PopupPredicted(Loc.GetString("eorg-action"), uid, uid, PopupType.LargeCaution);
+        args.Cancelled = true;
+    }
+
+    private void OnBeforeInteractHand(EntityUid uid, EorgPreventHandInteractComponent component, ref BeforeInteractHandEvent args)
+    {
+        if (!IsEnabled || !HasRoundEnded) return;
+        if (!HasComp<PreventEorgComponent>(uid)) return;
+
+        _popup.PopupPredicted(Loc.GetString("eorg-action"), uid, uid, PopupType.LargeCaution);
+        args.Handled = true;
+    }
+
+    private void OnBeforeRangedInteract(EntityUid uid, EorgPreventRangedInteractComponent component, ref BeforeRangedInteractEvent args)
+    {
+        if (!IsEnabled || !HasRoundEnded) return;
+        if (!HasComp<PreventEorgComponent>(args.User)) return;
+
+        _popup.PopupPredicted(Loc.GetString("eorg-action"), uid, uid, PopupType.LargeCaution);
+        args.Handled = true;
+    }
+
+    /// <summary>
+    /// Prevents performing actions marked as <see cref="EorgPreventActionComponent"/> if the user has
+    /// <see cref="PreventEorgComponent"/>.
+    /// </summary>
+    private void OnValidateAction(EntityUid uid, EorgPreventActionComponent component, ref ActionValidateEvent args)
+    {
+        if (!IsEnabled || !HasRoundEnded) return;
+        if (!HasComp<PreventEorgComponent>(args.User))  return;
+
+        _popup.PopupPredicted(Loc.GetString("eorg-action"), args.User, args.User, PopupType.LargeCaution);
+        args.Invalid = true;
+    }
+
+}
+
+[Serializable, NetSerializable]
+public sealed class RequestPreventEorgStateEvent : EntityEventArgs
+{
+}
+
+[Serializable, NetSerializable]
+public sealed class PreventEorgStateEvent(bool isEnabled, bool isRoundEnded) : EntityEventArgs
+{
+    public bool IsEnabled { get; } = isEnabled;
+    public bool IsRoundEnded { get; } = isRoundEnded;
+}
