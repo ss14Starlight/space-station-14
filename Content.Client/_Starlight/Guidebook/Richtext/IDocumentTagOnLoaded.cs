5ca17ebd6326b854ef64fae39b11dfb0ce1ecb06
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2026-04-18 09:19:51 -0700

    Fix links in chemicals guidebook (#4195)
    ## Short description
    <!-- What do you propose to change with your PR? -->
    Fixes the bug introduced in
    https://github.com/ss14Starlight/space-station-14/pull/3989
    
    ## Why we need to add this
    <!-- What is the reason for adding these changes? Please post links to
    Discussions as well as Bug Reports here. Please describe how this will
    change the game balance. -->
    Since the chemicals guidebook now loads progressively, the controls
    aren't there when it tries to link them
    This adds an interface to check for that adds an event when they finish
    loading
    
    ## Media (Video/Screenshots)
    <!--
    If your PR contains in-game changes you must provide screenshots/videos
    of the changes.
    -->
    
    ## Checks
    <!-- check boxes for faster reviewing of your PR -->
    
    - [x] I do not require assistance to complete the PR.
    - [x] Before posting/requesting review of a PR, I have verified that the
    changes work.
    - [x] I have added screenshots/videos of the changes, or this PR does
    not change in-game mechanics.
    - [x] I affirm that my changes are licensed under the [MIT
    License](https://github.com/ss14Starlight/space-station-14/blob/Starlight/LICENSE.TXT)
    and grant permission for use in this repository under its conditions.
    
    **Changelog**
    :cl:
    - fix: Fixed the chemicals in the Chemicals guidebook page not linking
    to each other.
    
    Co-authored-by: Starlight <ss14-Starlight@outlook.com>

---
+++ Content.Client/_Starlight/Guidebook/Richtext/IDocumentTagOnLoaded.cs
@@ -0,0 +1,11 @@
+﻿namespace Content.Client._Starlight.Guidebook.Richtext;
+
+public interface IDocumentTagOnLoaded
+{
+    /// <summary>
+    ///     Invoked by a control after it's done loading its controls.
+    ///     Used in cases such as the chemistry guidebook, where there's too many
+    ///     controls to load in a single frame.
+    /// </summary>
+    event Action? OnLoaded;
+}
