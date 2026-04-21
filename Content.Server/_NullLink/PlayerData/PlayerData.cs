de0b115fc6f14cb4ff01877e6df690610001cb33
Rinary <rinary.super@gmail.com>
2026-04-21 21:11:15 +0300

    Fix Resources property declaration in PlayerData

--- Content.Server/_NullLink/PlayerData/PlayerData.cs
+++ Content.Server/_NullLink/PlayerData/PlayerData.cs
@@ -12,7 +12,6 @@ public sealed class PlayerData
     public string? Title { get; set; }
     public required ICommonSession Session { get; init; }
     public ImmutableHashSet<ulong> Roles { get; set; } = [];
-    public Dictionary<string, double> Resources = [];
     public Dictionary<string, double> Resources { get; set; } = [];
     public Dictionary<string, Dictionary<string, TimeSpan>> RolePlayTimePerServer { get; set; } = [];
     public ulong DiscordId { get; set; }
