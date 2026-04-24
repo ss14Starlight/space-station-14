71eb5715a78d539710ba134dd1888dae0dec80c0
Your Name <you@example.com>
2026-04-24 22:43:52 +1000

    Remove ComponentReference

--- Content.Server/Nyanotrasen/Shipyard/Components/ShipyardConsoleComponent.cs
+++ Content.Server/Nyanotrasen/Shipyard/Components/ShipyardConsoleComponent.cs
@@ -3,5 +3,6 @@ using Content.Shared.Shipyard.Components;
 namespace Content.Server.Shipyard.Components;
 
 [RegisterComponent]
-[ComponentReference(typeof(SharedShipyardConsoleComponent))]
-public sealed partial class ShipyardConsoleComponent : SharedShipyardConsoleComponent {}
+public sealed partial class ShipyardConsoleComponent : SharedShipyardConsoleComponent
+{
+}
