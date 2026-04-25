4d0d3083056b92efd0b329d1bcdc42c2ed99911b
Your Name <you@example.com>
2026-04-25 18:11:45 +1000

    Remove register

--- Content.Client/_Starlight/Shipyard/Components/ShipyardConsoleComponent.cs
+++ Content.Client/_Starlight/Shipyard/Components/ShipyardConsoleComponent.cs
@@ -2,7 +2,6 @@ using Content.Shared._Starlight.Shipyard.Components;
 
 namespace Content.Client._Starlight.Shipyard.Components;
 
-[RegisterComponent]
 public sealed partial class ShipyardConsoleComponent : SharedShipyardConsoleComponent
 {
 }
