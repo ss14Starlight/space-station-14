5e7c81005892a132daa6d45750eba8db912e8c14
BigfootBravo <73325910+BigfootBravo@users.noreply.github.com>
2026-04-26 14:03:34 -0700

    Welding Works.

--- Content.Shared/Repairable/RepairableSystem.cs
+++ Content.Shared/Repairable/RepairableSystem.cs
@@ -7,6 +7,8 @@ using Content.Shared.Interaction;
 using Content.Shared.Popups;
 using Content.Shared.Tools.Systems;
 using Robust.Shared.Serialization;
+using Content.Shared.Body.Components;
+using Content.Shared._Starlight.Medical.Body.Systems;
 
 namespace Content.Shared.Repairable;
 
@@ -16,6 +18,7 @@ public sealed partial class RepairableSystem : EntitySystem
     [Dependency] private readonly DamageableSystem _damageableSystem = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
     [Dependency] private readonly ISharedAdminLogManager _adminLogger = default!;
+    [Dependency] private readonly SharedBloodstreamSystem _bloodstreamSystem = default!;
 
     public override void Initialize()
     {
@@ -94,8 +97,10 @@ public sealed partial class RepairableSystem : EntitySystem
         if (args.Handled)
             return;
 
-        // Only try repair the target if it is damaged
-        if (!TryComp<DamageableComponent>(ent.Owner, out var damageable) || damageable.TotalDamage == 0)
+        // Only try repair the target if it is damaged or bleeding
+        bool isBleeding = TryComp<BloodstreamComponent>(ent, out var bloodstream) && bloodstream.BleedAmount > 0; //for beings that are repaired with the welder, but can bleed (like IPCs)
+
+        if ((!TryComp<DamageableComponent>(ent.Owner, out var damageable) || damageable.TotalDamage == 0) && !isBleeding)
             return;
 
         #region Starlight
