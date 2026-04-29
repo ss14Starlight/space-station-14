f3719c19e31334308e38cfe1701a8b0d71946f7a
neurovermi <207633452+neurovermi@users.noreply.github.com>
2026-04-29 05:50:31 +0000

    arrow-ize ElectrocutionAttempEvent handling function
    Co-authored-by: Rinary <rinary.super@gmail.com>

--- Content.Shared/Mech/EntitySystems/SharedMechSystem.cs
+++ Content.Shared/Mech/EntitySystems/SharedMechSystem.cs
@@ -715,10 +715,8 @@ public abstract partial class SharedMechSystem : EntitySystem
         args.Cancelled = args.User != uid;
     }
 
-    private void OnMechPilotElectrocutionAttempt(EntityUid uid, MechPilotComponent comp, ElectrocutionAttemptEvent args)
-    {
-        args.SiemensCoefficient *= 0f; // Fully insulate the pilot
-    }
+    private void OnMechPilotElectrocutionAttempt(EntityUid uid, MechPilotComponent comp, ElectrocutionAttemptEvent args) 
+        => args.SiemensCoefficient *= 0f; // Fully insulate the pilot
     #endregion
 }
 
