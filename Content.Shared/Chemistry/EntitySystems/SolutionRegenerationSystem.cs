b2de8b7ffddad2370a5bc39ecac06adf52dc871f
Princess Cheeseballs <66055347+Princess-Cheeseballs@users.noreply.github.com>
2026-05-01 14:20:03 -0700

    Fix Broken Solution Enumerators (#43789)
    * burge
    
    * bruegr
    
    * YAML
    
    * two misc fixes
    
    * snoutta here
    
    * add tests
    
    ---------
    
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
    Co-authored-by: Janet Blackquill <uhhadd@gmail.com>
    # Conflicts:
    #	Content.Server/Chemistry/EntitySystems/VaporSystem.cs
    #	Content.Server/Fluids/EntitySystems/SpraySystem.cs
    #	Content.Server/Weapons/Ranged/Systems/GunSystem.Solution.cs
    #	Content.Shared/Chemistry/EntitySystems/SolutionPurgeSystem.cs
    #	Content.Shared/Chemistry/EntitySystems/SolutionRegenerationSystem.cs
    #	Content.Shared/EntityEffects/Effects/Solution/AddReagentToSolutionEntityEffectSystem.cs
    #	Resources/Prototypes/Entities/Objects/Tools/lighters.yml
    #	Resources/Prototypes/Entities/Structures/Furniture/toilet.yml

--- Content.Shared/Chemistry/EntitySystems/SolutionRegenerationSystem.cs
+++ Content.Shared/Chemistry/EntitySystems/SolutionRegenerationSystem.cs
@@ -17,7 +17,6 @@ public sealed partial class SolutionRegenerationSystem : EntitySystem
         base.Initialize();
 
         SubscribeLocalEvent<SolutionRegenerationComponent, MapInitEvent>(OnMapInit);
-        SubscribeLocalEvent<SolutionRegenerationComponent, EntRemovedFromContainerMessage>(OnEntRemoved);
     }
 
     private void OnMapInit(Entity<SolutionRegenerationComponent> ent, ref MapInitEvent args)
@@ -27,14 +26,6 @@ public sealed partial class SolutionRegenerationSystem : EntitySystem
         Dirty(ent);
     }
 
-    // Workaround for https://github.com/space-wizards/space-station-14/pull/35314
-    private void OnEntRemoved(Entity<SolutionRegenerationComponent> ent, ref EntRemovedFromContainerMessage args)
-    {
-        // Make sure the removed entity was our contained solution and clear our cached reference
-        if (args.Entity == ent.Comp.SolutionRef?.Owner)
-            ent.Comp.SolutionRef = null;
-    }
-
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
@@ -42,8 +33,8 @@ public sealed partial class SolutionRegenerationSystem : EntitySystem
         // Starlight start
         var time = _timing.CurTime;
         // TODO: SolutionRegenerationComponent on Solution Entities!
-        var query = EntityQueryEnumerator<SLActiveSolutionRegenerationComponent, SolutionRegenerationComponent, SolutionManagerComponent>();
-        while (query.MoveNext(out var uid, out _, out var regen, out var manager))
+        var query = EntityQueryEnumerator<SLActiveSolutionRegenerationComponent, SolutionRegenerationComponent, SolutionComponent>();
+        while (query.MoveNext(out var uid, out _, out var regen, out var solution))
         {
             if (time < regen.NextRegenTime)
                 continue;
@@ -53,13 +44,7 @@ public sealed partial class SolutionRegenerationSystem : EntitySystem
             regen.NextRegenTime = time + regen.Duration;
             // Needs to be networked and dirtied so that the client can reroll it during prediction
             Dirty(uid, regen);
-            if (!_solutionContainer.ResolveSolution((uid, manager),
-                    regen.SolutionName,
-                    ref regen.SolutionRef,
-                    out var solution))
-                continue;
-
-            var amount = FixedPoint2.Min(solution.AvailableVolume, regen.Generated.Volume);
+            var amount = FixedPoint2.Min(solution.Solution.AvailableVolume, regen.Generated.Volume);
             if (amount <= FixedPoint2.Zero)
             {
                 RemCompDeferred<SLActiveSolutionRegenerationComponent>(uid);
@@ -72,7 +57,7 @@ public sealed partial class SolutionRegenerationSystem : EntitySystem
                 ? regen.Generated
                 : regen.Generated.Clone().SplitSolution(amount);
 
-            _solutionContainer.TryAddSolution(regen.SolutionRef.Value, generated);
+            _solutionContainer.TryAddSolution((uid, solution), generated);
         }
     }
 }
