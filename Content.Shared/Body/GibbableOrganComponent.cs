d7fb4bf334a251929346dd547d51b210433d165c
Your Name <you@example.com>
2026-05-05 17:29:07 +1000

    Remove commented out files

--- Content.Shared/Body/GibbableOrganComponent.cs
+++
@@ -1,9 +0,0 @@
-// Starlight Edit: This has been commented out since we dont use NuBody
-//
-// using Robust.Shared.GameStates;
-
-// namespace Content.Shared.Body;
-
-// [RegisterComponent, NetworkedComponent]
-// [Access(typeof(GibbableOrganSystem))]
-// public sealed partial class GibbableOrganComponent : Component;
