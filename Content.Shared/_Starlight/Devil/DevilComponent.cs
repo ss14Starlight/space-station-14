16d4cfcdee400d6bba65db2d64aff499613f29a5
Arkanic <50847107+Arkanic@users.noreply.github.com>
2026-05-09 13:01:45 +1200

    whoops

--- Content.Shared/_Starlight/Devil/DevilComponent.cs
+++ Content.Shared/_Starlight/Devil/DevilComponent.cs
@@ -43,8 +43,7 @@ public sealed partial class DevilComponent : Component
         "Gun",
         "Electricity",
         "Noslip",
-        "Mute",
-        "TestFail"
+        "Mute"
     };
 
     /// <summary>
