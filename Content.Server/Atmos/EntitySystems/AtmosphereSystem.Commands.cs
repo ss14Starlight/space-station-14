cdd75c356e03a6db25c389e043c32737ceb01a93
Ruroa <progamerlp12@gmail.com>
2026-05-18 01:28:52 +0200

    newgasfixmarker
    New Atmos gas fix marker so outside is acctully CO2

--- Content.Server/Atmos/EntitySystems/AtmosphereSystem.Commands.cs
+++ Content.Server/Atmos/EntitySystems/AtmosphereSystem.Commands.cs
@@ -68,7 +68,7 @@ public sealed partial class AtmosphereSystem
     /// <remarks>Please be responsible with this method. Used only by tests and fixgridatmos.</remarks>
     public void RebuildGridAtmosphere(Entity<GridAtmosphereComponent, MapGridComponent> ent)
     {
-        var mixtures = new GasMixture[9];
+        var mixtures = new GasMixture[10];
         for (var i = 0; i < mixtures.Length; i++)
         {
             mixtures[i] = new GasMixture(Atmospherics.CellVolume) { Temperature = Atmospherics.T20C };
@@ -106,6 +106,11 @@ public sealed partial class AtmosphereSystem
         mixtures[8].AdjustMoles(Gas.Oxygen, Atmospherics.OxygenMolesGasMiner);
         mixtures[8].AdjustMoles(Gas.Nitrogen, Atmospherics.NitrogenMolesGasMiner);
 
+        // 9: Verdant outside atmosphere, matching the map atmosphere mix.
+        mixtures[9].AdjustMoles(Gas.Oxygen, 5f);
+        mixtures[9].AdjustMoles(Gas.Nitrogen, 5f);
+        mixtures[9].AdjustMoles(Gas.CarbonDioxide, 100f);
+        mixtures[9].Temperature = 288f;
 
         // Force Invalidate & update air on all tiles
         Entity<GridAtmosphereComponent, GasTileOverlayComponent, MapGridComponent, TransformComponent> grid =
