d46b164ab5c965cbe7c5d3aa0619293fbb76ef89
Maitake <heyitsmaitake@gmail.com>
2026-05-19 14:03:18 -0700

    Merge pull request #19 from DivineHoBo/Plastitanium-Door
    Added Plastitanium Door

---
+++ Content.Server/_Blimpuf/PlastitaniumDoor/UntamperableComponent.cs
@@ -0,0 +1,7 @@
+﻿namespace Content.Server._Blimpuf.PlastitaniumDoor
+{
+    [RegisterComponent]
+    public sealed partial class UntamperableComponent : Component
+    {
+    }
+}
---
+++ Content.Server/_Blimpuf/PlastitaniumDoor/UntamperableSystem.cs
@@ -0,0 +1,31 @@
+﻿using Content.Server.Doors.Systems;
+using Content.Shared.Emag.Systems;
+using Content.Shared.Interaction;
+using Content.Shared.Tools.Components;
+
+namespace Content.Server._Blimpuf.PlastitaniumDoor
+{
+    public sealed class UntamperableSystem : EntitySystem
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            SubscribeLocalEvent<UntamperableComponent, GotEmaggedEvent>(OnEmagged);
+            SubscribeLocalEvent<UntamperableComponent, InteractUsingEvent>(OnInteractUsing, before: new[] { typeof(DoorSystem) });
+        }
+
+        private void OnEmagged(EntityUid uid, UntamperableComponent component, ref GotEmaggedEvent args)
+        {
+            args.Handled = true;
+        }
+
+        private void OnInteractUsing(EntityUid uid, UntamperableComponent component, InteractUsingEvent args)
+        {
+            if (HasComp<ToolComponent>(args.Used))
+            {
+                args.Handled = true;
+            }
+        }
+    }
+}
