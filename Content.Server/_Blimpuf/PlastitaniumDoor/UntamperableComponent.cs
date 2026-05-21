5791e82bd23f9cb3a89696e95e4b1b9c8cc5700f
Maitake <heyitsmaitake@gmail.com>
2026-05-21 00:19:47 -0700

    Merge pull request #20 from DivineHoBo/Plastitanium-Door
    Plastitanium Door Update

--- Content.Server/_Blimpuf/PlastitaniumDoor/UntamperableComponent.cs
+++ Content.Server/_Blimpuf/PlastitaniumDoor/UntamperableComponent.cs
@@ -1,7 +1,12 @@
-﻿namespace Content.Server._Blimpuf.PlastitaniumDoor
+﻿using Robust.Shared.Audio;
+
+namespace Content.Server._Blimpuf.PlastitaniumDoor
 {
     [RegisterComponent]
     public sealed partial class UntamperableComponent : Component
     {
+        [DataField("disabled")] public bool AccessChangeDisabled = false;
+
+        [DataField("denyChangeSound")] public SoundSpecifier DenyChangeSound = new SoundPathSpecifier("/Audio/Machines/airlock_deny.ogg");
     }
 }
--- Content.Server/_Blimpuf/PlastitaniumDoor/UntamperableSystem.cs
+++ Content.Server/_Blimpuf/PlastitaniumDoor/UntamperableSystem.cs
@@ -1,18 +1,22 @@
 ﻿using Content.Server.Doors.Systems;
+using Content.Shared.Access.Components;
 using Content.Shared.Emag.Systems;
 using Content.Shared.Interaction;
 using Content.Shared.Tools.Components;
+using Robust.Shared.Audio.Systems;
 
 namespace Content.Server._Blimpuf.PlastitaniumDoor
 {
-    public sealed class UntamperableSystem : EntitySystem
+    public sealed partial class UntamperableSystem : EntitySystem
     {
+        [Dependency] private readonly SharedAudioSystem _audio = default!;
         public override void Initialize()
         {
             base.Initialize();
 
             SubscribeLocalEvent<UntamperableComponent, GotEmaggedEvent>(OnEmagged);
             SubscribeLocalEvent<UntamperableComponent, InteractUsingEvent>(OnInteractUsing, before: new[] { typeof(DoorSystem) });
+            SubscribeLocalEvent<UntamperableComponent, AccessReaderConfigurationAttemptEvent>(OnConfigAttempt);
         }
 
         private void OnEmagged(EntityUid uid, UntamperableComponent component, ref GotEmaggedEvent args)
@@ -24,7 +28,16 @@ namespace Content.Server._Blimpuf.PlastitaniumDoor
         {
             if (HasComp<ToolComponent>(args.Used))
             {
+                _audio.PlayPvs(component.DenyChangeSound, uid);
                 args.Handled = true;
+            }
+        }
+        private void OnConfigAttempt(EntityUid uid, UntamperableComponent component, AccessReaderConfigurationAttemptEvent args)
+        {
+            if (component.AccessChangeDisabled)
+            {
+                _audio.PlayPvs(component.DenyChangeSound, uid);
+                args.Cancel();
             }
         }
     }
