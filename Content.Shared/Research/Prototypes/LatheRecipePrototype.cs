80045dc3a6e69ed0071c7499f4f4d025e7e1706c
wonderfulnewworld <shinsekaihibiki@gmail.com>
2026-05-20 22:53:45 -0700

    comment

--- Content.Shared/Research/Prototypes/LatheRecipePrototype.cs
+++ Content.Shared/Research/Prototypes/LatheRecipePrototype.cs
@@ -46,7 +46,7 @@ namespace Content.Shared.Research.Prototypes
         public bool PrintTicket = false;
 
         [DataField]
-        public EntProtoId TicketProtoId = "SalvageTicket"; // Starlight
+        public EntProtoId TicketProtoId = "SalvageTicket";
 
         [DataField]
         public Dictionary<ProtoId<ReagentPrototype>, FixedPoint2>? ResultReagents;
