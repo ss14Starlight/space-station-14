2adc3504343b1998509edc0e5723bd5e7c6ab6fa
JohnnyMalawny <johnnymalawny@gmail.com>
2026-06-12 19:37:48 -0700

    grid POWA

--- Content.IntegrationTests/Tests/_Starlight/Power/GridPowerTests.cs
+++ Content.IntegrationTests/Tests/_Starlight/Power/GridPowerTests.cs
@@ -94,6 +94,7 @@ public sealed class GridPowerTests
         new("/Maps/_Starlight/Shuttles/barge.yml"),
         new("/Maps/_Starlight/Shuttles/prospector.yml"),
         new("/Maps/_Starlight/Shuttles/emergency_lox.yml")
+        new("/Maps/_Starlight/Shuttles/Comet.yml")
     ];
 
     [Test, TestCaseSource(nameof(GridPaths))]
