ace441152626128314874b2d75a7b60eafc9841b
CawsForConcern <kopczynski.zesty@gmail.com>
2026-06-16 15:44:12 -0700

    step 1: marking eyes as disability blockable for night vision

--- Content.Shared/_Starlight/Overlay/Components/NightVisionComponent.cs
+++ Content.Shared/_Starlight/Overlay/Components/NightVisionComponent.cs
@@ -9,6 +9,13 @@ public sealed partial class NightVisionComponent : Component
     [DataField, AutoNetworkedField]
     public bool Active = true;
 
+    /// <summary>
+    /// Whether or not the night vision provided by this component is blocked by disabilities.
+    /// Special organs like cybereyes bypass disabilities, while fleshy default organs won't.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool DisabilityBlockable = false;
+
     [DataField]
     public EntProtoId EffectPrototype = "EffectNightVision";
 
