f2d40136c401da564fb4b62020474ab4d51e18f6
Your Name <you@example.com>
2026-06-16 16:29:00 +1000

    Fix

--- Content.Server/Temperature/Systems/TemperatureSystem.Damage.cs
+++ Content.Server/Temperature/Systems/TemperatureSystem.Damage.cs
@@ -35,6 +35,8 @@ public sealed partial class TemperatureSystem
     /// </summary>
     public HashSet<Entity<TemperatureDamageComponent>> ShouldUpdateDamage = new();
 
+    private readonly List<Entity<TemperatureDamageComponent>> _shouldUpdateDamageBuffer = []; // Starlight
+
     /// <summary>
     /// Alert prototype for Temperature.
     /// </summary>
@@ -69,19 +71,30 @@ public sealed partial class TemperatureSystem
 
     private void UpdateDamage()
     {
-        foreach (var entity in ShouldUpdateDamage)
+        #region Starlight Edit
+        // Starlight Edit: Collection modified during Enumeration fix
+        _shouldUpdateDamageBuffer.Clear();
+        _shouldUpdateDamageBuffer.AddRange(ShouldUpdateDamage);
+        ShouldUpdateDamage.Clear();
+
+        foreach (var entity in _shouldUpdateDamageBuffer)
         {
             if (Deleted(entity) || Paused(entity))
                 continue;
 
-            var deltaTime = _gameTiming.CurTime - entity.Comp.LastUpdate;
-            if (entity.Comp.TakingDamage && deltaTime < entity.Comp.UpdateInterval)
+            if (!TryComp<TemperatureDamageComponent>(entity.Owner, out var tempDamage))
                 continue;
 
-            ChangeDamage(entity, deltaTime);
+            var deltaTime = _gameTiming.CurTime - tempDamage.LastUpdate;
+
+            if (tempDamage.TakingDamage && deltaTime < tempDamage.UpdateInterval)
+                continue;
+
+            ChangeDamage((entity.Owner, tempDamage), deltaTime);
+        #endregion
         }
 
-        ShouldUpdateDamage.Clear();
+        // ShouldUpdateDamage.Clear(); // Starlight Edit: Moved up
     }
 
     private void ChangeDamage(Entity<TemperatureDamageComponent> entity, TimeSpan deltaTime)
