dbb5f0584032767c9cfd84fd4bbd7681a58794b7
Your Name <you@example.com>
2026-06-20 01:14:10 +1000

    Merge Starlight-Dev

---
+++ Content.Shared/_Starlight/Shuttles/BUIStates/DockingPortStates.cs
@@ -0,0 +1,12 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared.Shuttles.BUIStates;
+
+/// <summary>
+/// Wrapper for the dictionary of docking port states.
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class DockingPortStates(Dictionary<NetEntity, List<DockingPortState>> docks)
+{
+    public Dictionary<NetEntity, List<DockingPortState>> Docks = docks;
+}
