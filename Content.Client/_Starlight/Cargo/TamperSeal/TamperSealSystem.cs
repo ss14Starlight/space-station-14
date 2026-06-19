dc55b6babbc154c188da90964bfbf7c109b071b4
Red Mushie <noreply@redmushie.me>
2026-06-19 16:38:15 +0200

    Tidyyy

--- Content.Client/_Starlight/Cargo/TamperSeal/TamperSealSystem.cs
+++ Content.Client/_Starlight/Cargo/TamperSeal/TamperSealSystem.cs
@@ -2,7 +2,5 @@ using Content.Shared._Starlight.Cargo.TamperSeal;
 
 namespace Content.Client._Starlight.Cargo.TamperSeal;
 
-/// <summary>
 /// <inheritdoc/>
-/// </summary>
 public sealed class TamperSealSystem : SharedTamperSealSystem;
