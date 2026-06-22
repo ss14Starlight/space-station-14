4ba150c983387355d63646194b9762f06a4e63a0
Red Mushie <82113471+redmushie@users.noreply.github.com>
2026-06-22 09:57:00 +0200

    Update Content.Shared/_Starlight/Medical/Body/Systems/SharedBodySystem.Organs.cs

--- Content.Shared/_Starlight/Medical/Body/Systems/SharedBodySystem.Organs.cs
+++ Content.Shared/_Starlight/Medical/Body/Systems/SharedBodySystem.Organs.cs
@@ -210,6 +210,10 @@ public partial class SharedBodySystem
         comps = null;
         return false;
     }
+
+    /// <summary>
+    /// Checks if the slot with the given name exists on the body. Does NOT check if an organ is present in the slot.
+    /// </summary>
     public bool HasOrganSlot(EntityUid bodyId, BodyComponent body, string slotId)
     {
         foreach (var (_, partComp) in GetBodyChildren(bodyId, body))
