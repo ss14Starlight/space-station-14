319d04ca5607937be752e5ed6b2c28532f0954a5
neomoth <69372252+neomoth@users.noreply.github.com>
2026-06-22 18:33:28 -0400

    working

--- Content.Server/Administration/Commands/AGhostCommand.cs
+++ Content.Server/Administration/Commands/AGhostCommand.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using Content.Server.GameTicking;
 using Content.Server.Ghost;
 using Content.Server.Mind;
+using Content.Shared._Starlight.Administration.Events;
 using Content.Shared.Administration;
 using Content.Shared.Ghost;
 using Content.Shared.Mind;
@@ -118,5 +119,9 @@ public sealed partial class AGhostCommand : LocalizedCommands
 
         var comp = _entities.GetComponent<GhostComponent>(ghost);
         ghostSystem.SetCanReturnToBody((ghost, comp), canReturn);
+
+        // Starlight begin
+        _entities.EntityNetManager.SendSystemNetworkMessage(new AdminGhostEvent(), shell.Player!.Channel);
+        // Starlight end
     }
 }
