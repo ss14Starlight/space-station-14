94040946842bbb2f8fd1cd1d9dd0b1e11d6fe65a
wonderfulnewworld <shinsekaihibiki@gmail.com>
2026-06-22 00:34:40 -0700

    zxa requires water vapor now

--- Content.Server/_Starlight/Atmos/Reactions/ZXAProductionReaction.cs
+++ Content.Server/_Starlight/Atmos/Reactions/ZXAProductionReaction.cs
@@ -20,12 +20,13 @@ public sealed partial class ZXAProductionReaction : IGasReactionEffect
 
         var initN2O = mixture.GetMoles(Gas.NitrousOxide);
         var initBZ = mixture.GetMoles(Gas.BZ);
+        var initVapor = mixture.GetMoles(Gas.WaterVapor);
         var pressure = mixture.Pressure;
         var volume = mixture.Volume;
 
         var environmentEfficiency = volume / pressure; // more volume and less pressure gives better rates
         var ratioEfficiency = Math.Min(initN2O / initBZ, 1f); // less n2o than BZ gives lower rates
-        var zxaFormed = Math.Min(0.02f * ratioEfficiency * environmentEfficiency, Math.Min(initN2O * 2.25f, initBZ * 1f));
+        var zxaFormed = Math.Min(0.02f * ratioEfficiency * environmentEfficiency, Math.Min(initN2O * 2.25f, Math.Min(initBZ * 1f, initVapor * 1.5f)));
 
         var nitrousOxideDecomposed = Math.Max(4f * (initBZ / (initN2O + initBZ) - 0.75f), 0);
         var nitrogenAdded = 0f;
@@ -39,12 +40,14 @@ public sealed partial class ZXAProductionReaction : IGasReactionEffect
         var zxaAdded = zxaFormed * (1f - nitrousOxideDecomposed);
         var n2oRemoved = zxaFormed / 2.25f;
         var bzRemoved = zxaFormed * (1f - nitrousOxideDecomposed);
+        var vaporRemoved = zxaFormed / 3f; // Half of the water vapor is returned, it's a semi-catalyst.
 
-        if (n2oRemoved > initN2O || bzRemoved > initBZ)
+        if (n2oRemoved > initN2O || bzRemoved > initBZ|| vaporRemoved > initVapor)
             return ReactionResult.NoReaction;
 
         mixture.AdjustMoles(Gas.NitrousOxide, -n2oRemoved);
         mixture.AdjustMoles(Gas.BZ, -bzRemoved);
+        mixture.AdjustMoles(Gas.WaterVapor, -vaporRemoved);
         mixture.AdjustMoles(Gas.Nitrogen, nitrogenAdded);
         mixture.AdjustMoles(Gas.Oxygen, oxygenAdded);
         mixture.AdjustMoles(Gas.ZXA, zxaAdded);
