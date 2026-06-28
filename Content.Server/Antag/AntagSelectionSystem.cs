3b77773655069ea007c0850f01576a4ff9db6e18
wonderfulnewworld <shinsekaihibiki@gmail.com>
2026-06-28 11:10:49 -0700

    the other pr

--- Content.Server/Antag/AntagSelectionSystem.cs
+++ Content.Server/Antag/AntagSelectionSystem.cs
@@ -45,6 +45,7 @@ using Robust.Shared.Physics.Systems;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Physics;
 using Content.Shared.Hands.Components;
+using Content.Shared.Humanoid;
 // Starlight End
 using static Content.Server.Antag.Components.AntagSelectionTime;
 
@@ -327,24 +328,22 @@ public sealed partial class AntagSelectionSystem : GameRuleSystem<AntagSelection
         }
     }
 
-    private AntagCount[]  GetAntags(Entity<AntagSelectionComponent> gameRule,
+    private List<AntagCount>  GetAntags(Entity<AntagSelectionComponent> gameRule,
         int playerCount)
     {
         var runningCount = 0;
-        var antags = new AntagCount[gameRule.Comp.Antags.Length];
+        var antags = new List<AntagCount>(gameRule.Comp.Antags.Length);
 
         // We assume that antag definitions are prioritized by order, and take up slots that other roles may take.
         // I.E for Nukies, it selects 1 commander which takes up 10 players, then one corpsman which takes up another 10, then we select X nukies based on the remaining player count.
         // This is how the system worked when I got here, and I decided not to change it to avoid fucking with team antag balance
-        var i = 0;
         foreach (var antag in gameRule.Comp.Antags)
         {
             if (!Proto.Resolve(antag.Proto, out var definition))
                 continue;
 
             // We do it this way in case our resolve fails.
-            antags[i] = (definition, GetTargetAntagCount(antag, playerCount, ref runningCount));
-            i++;
+            antags.Add((definition, GetTargetAntagCount(antag, playerCount, ref runningCount)));
         }
 
         return antags;
@@ -379,19 +378,19 @@ public sealed partial class AntagSelectionSystem : GameRuleSystem<AntagSelection
         gameRule.Comp.PreSelectionsComplete = true;
     }
 
-    private void AssignAntags(Entity<AntagSelectionComponent> gameRule, IList<ICommonSession> players, AntagCount[] antags)
+    private void AssignAntags(Entity<AntagSelectionComponent> gameRule, IList<ICommonSession> players, List<AntagCount> antags)
     {
         AssignAntags(gameRule, GetWeightedPlayerPool(players), antags);
     }
 
-    private void AssignAntags(Entity<AntagSelectionComponent> gameRule, Dictionary<ICommonSession, float> weightedPool, AntagCount[] antags)
+    private void AssignAntags(Entity<AntagSelectionComponent> gameRule, Dictionary<ICommonSession, float> weightedPool, List<AntagCount> antags)
     {
         while (RobustRandom.TryPickAndTake(weightedPool, out var session))
         {
             AssignAntag(gameRule, session, ref antags);
 
             // Assignment complete, return early.
-            if (antags.Length == 0)
+            if (antags.Count == 0)
                 return;
         }
 
@@ -515,13 +514,13 @@ public sealed partial class AntagSelectionSystem : GameRuleSystem<AntagSelection
     /// Selects and assigns antags from a list.
     /// Is private because it has it should only ever be run in very specific scenarios.
     /// </summary>
-    private bool AssignAntag(Entity<AntagSelectionComponent> gameRule, ICommonSession player, ref AntagCount[] antags)
+    private bool AssignAntag(Entity<AntagSelectionComponent> gameRule, ICommonSession player, ref List<AntagCount> antags)
     {
         // If this session cannot be an antag, then get the next session!
         if (!TryGetValidAntagPreferences(player, out var prefs))
             return false;
 
-        for (var i = antags.Length - 1; i >= 0; i--)
+        for (var i = antags.Count - 1; i >= 0; i--)
         {
             var antag = antags[i];
 
