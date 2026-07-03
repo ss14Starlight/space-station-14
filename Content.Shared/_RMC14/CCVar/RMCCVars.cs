3bde19dc7f9b7e598b322a5bf5cc3d24790b9d47
Conflee <46701008+Conflee@users.noreply.github.com>
2026-07-02 20:05:16 -0400

    New Player Icons

---
+++ Content.Shared/_RMC14/CCVar/RMCCVars.cs
@@ -0,0 +1,17 @@
+using Robust.Shared;
+using Robust.Shared.Configuration;
+
+namespace Content.Shared._RMC14.CCVar;
+
+[CVarDefs]
+public sealed partial class RMCCVars : CVars
+{
+    public static readonly CVarDef<int> RMCNewPlayerTimeTotalHours =
+        CVarDef.Create("rmc.new_player_time_total_hours", 20, CVar.REPLICATED | CVar.SERVER);
+
+    public static readonly CVarDef<int> RMCNewPlayerTimeJobHours =
+        CVarDef.Create("rmc.new_player_time_job_hours", 10, CVar.REPLICATED | CVar.SERVER);
+
+    public static readonly CVarDef<int> RMCBrandNewPlayerTimeJobHours =
+        CVarDef.Create("rmc.brand_new_player_time_job_hours", 1, CVar.REPLICATED | CVar.SERVER);
+}
---
+++ Content.Shared/_RMC14/NewPlayer/NewPlayerLayers.cs
@@ -0,0 +1,18 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared._RMC14.NewPlayer;
+
+[Serializable, NetSerializable]
+public enum NewPlayerLayers
+{
+    Layer,
+}
+
+[Serializable, NetSerializable]
+public enum NewPlayerVisuals
+{
+    One,
+    Two,
+    Three,
+	Four,
+}
