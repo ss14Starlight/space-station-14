8c8c03d6d86843f1fb07a1e0e9860caf5a520002
Conflee <46701008+Conflee@users.noreply.github.com>
2026-07-02 22:34:23 -0400

    fixes

--- Content.Shared/_RMC14/CCVar/RMCCVars.cs
+++ Content.Shared/_RMC14/CCVar/RMCCVars.cs
@@ -14,4 +14,7 @@ public sealed partial class RMCCVars : CVars
 
     public static readonly CVarDef<int> RMCBrandNewPlayerTimeJobHours =
         CVarDef.Create("rmc.brand_new_player_time_job_hours", 1, CVar.REPLICATED | CVar.SERVER);
+
+    public static readonly CVarDef<bool> RMCShowNewPlayerIcons =
+        CVarDef.Create("rmc.show_new_player_icons", true, CVar.REPLICATED | CVar.CLIENT | CVar.ARCHIVE);
 }
