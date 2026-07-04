09b5c5e76773a5a6c047bc5d149f4cca5c06e0a2
Dennisz125 <denzabluda@gmail.com>
2026-07-03 22:30:13 -0700

    Initial Push

--- Content.IntegrationTests/Tests/_Starlight/Power/GridPowerTests.cs
+++ Content.IntegrationTests/Tests/_Starlight/Power/GridPowerTests.cs
@@ -89,6 +89,7 @@ public sealed class GridPowerTests
         new("/Maps/_Starlight/Shuttles/prospector.yml"),
         new("/Maps/_Starlight/Shuttles/emergency_lox.yml"),
         new("/Maps/_Starlight/Shuttles/Mini_Ingeniator.yml"),
+        new("/Maps/_Starlight/Shuttles/Bumblebee.yml"),
         new("/Maps/_Starlight/Shuttles/Comet.yml"),
         new("/Maps/_Starlight/Shuttles/Munchies.yml")
     ];
