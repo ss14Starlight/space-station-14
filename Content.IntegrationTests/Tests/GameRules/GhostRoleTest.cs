edc2e27c1ffb69e387ec272985212f64be75e016
wonderfulnewworld <shinsekaihibiki@gmail.com>
2026-07-13 20:28:49 -0700

    FINALLY. FIX. TESTS.

--- Content.IntegrationTests/Tests/GameRules/GhostRoleTest.cs
+++ Content.IntegrationTests/Tests/GameRules/GhostRoleTest.cs
@@ -8,6 +8,8 @@ using Content.Server.Antag.Components;
 using Content.Server.GameTicking;
 using Content.Server.Ghost.Roles;
 using Content.Server.Ghost.Roles.Components;
+using Content.Server.Players.PlayTimeTracking; // Starlight
+using Content.Shared._Starlight.CCVar; // Starlight
 using Content.Shared.Antag;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Map;
@@ -23,6 +25,7 @@ public sealed partial class GhostRoleTest : GameTest
     [SidedDependency(Side.Server)] private IRobustRandom _random = default!;
     [SidedDependency(Side.Server)] private GameTicker _ticker = default!;
     [SidedDependency(Side.Server)] private GhostRoleSystem _ghostRole = default!;
+    [SidedDependency(Side.Server)] private PlayTimeTrackingSystem _playTime = default!; // Starlight
 
     private static string[] _antagGameRules = GameDataScrounger.EntitiesWithComponent("AntagSelection");
 
@@ -41,61 +44,92 @@ public sealed partial class GhostRoleTest : GameTest
     [RunOnSide(Side.Server)]
     public void TestAntagGhostRoles(string ruleId)
     {
-        var rule = SProtoMan.Index<EntityPrototype>(ruleId);
-        Assert.That(rule.TryGetComponent<AntagSelectionComponent>(out var antag, SEntMan.ComponentFactory), Is.True);
+        #region Starlight
+        // The vast majority of this is from Wizden, but it's been moved around because we disable load map on tests. Alas...
+        // It's really just "Starlight" because of the Try-Finally ccvar stuff.
+        var serverCfg = Pair.Server.CfgMan;
+        serverCfg.SetCVar(StarlightCCVars.DisableLoadMapRule, false);
 
-        _ticker.StartGameRule(ruleId, out var gameRule);
-
-        Dictionary<ProtoId<AntagSpecifierPrototype>, int> rules = [];
-
-        foreach (var selector in antag!.Antags)
+        try
         {
-            var specifier = SProtoMan.Index(selector.Proto);
-            var count = selector.GetTargetAntagCount(_random, 1);
-            // We should always spawn at least one antag if we add a GameRule
-            Assert.That(count, Is.GreaterThan(0));
+            var rule = SProtoMan.Index<EntityPrototype>(ruleId);
+            Assert.That(rule.TryGetComponent<AntagSelectionComponent>(out var antag, SEntMan.ComponentFactory), Is.True);
 
-            if (specifier.SpawnerPrototype == null)
-                continue;
+            _ticker.StartGameRule(ruleId, out var gameRule);
 
-            var value = rules.GetValueOrDefault(specifier);
-            rules[selector.Proto] = value + count;
+            // Some rules can be ended before activation in integration configuration (e.g. map loading disabled).
+            // If the rule is not active, it cannot spawn antag ghost roles, so this case is not testable here.
+            if (!_ticker.IsGameRuleActive(gameRule))
+            {
+                _ticker.ClearGameRules();
+                Assert.That(_ticker.GetAddedGameRules(), Is.Empty);
+                return;
+            }
+
+            Dictionary<ProtoId<AntagSpecifierPrototype>, int> rules = [];
+
+            foreach (var selector in antag!.Antags)
+            {
+                var specifier = SProtoMan.Index(selector.Proto);
+                var count = selector.GetTargetAntagCount(_random, 1);
+                // Starlight - Some selectors (e.g. linear with min 0) validly return zero at low player counts.
+                Assert.That(count, Is.GreaterThanOrEqualTo(0)); // Starlight - GreaterThan -> GreaterThanOrEqualTo
+
+                if (specifier.SpawnerPrototype == null)
+                    continue;
+
+                var value = rules.GetValueOrDefault(specifier);
+                rules[selector.Proto] = value + count;
+            }
+
+            var roleEnumerator = SEntMan.EntityQueryEnumerator<GhostRoleAntagSpawnerComponent, GhostRoleComponent, TransformComponent>();
+            while (roleEnumerator.MoveNext(out var spawner, out var role, out var xform))
+            {
+                // Ensure the ghost role spawner spawned correctly!
+                Assert.That(spawner.Rule, Is.EqualTo(gameRule));
+                Assert.That(spawner.Definition, Is.Not.Null);
+                Assert.That(xform.MapUid, Is.Not.Null);
+                Assert.That(xform.MapID, Is.Not.EqualTo(MapId.Nullspace));
+
+                var value = rules[spawner.Definition.Value];
+                rules[spawner.Definition.Value] = value - 1;
+
+                var definition = SProtoMan.Index(spawner.Definition.Value);
+                var canTakeRole = _playTime.IsAllowed(ServerSession!, definition.PrefRoles);
+
+                // Take the ghost role. Some antags (e.g. Brighteye) have requirement gating.
+                var tookRole = _ghostRole.Takeover(ServerSession!, role.Identifier);
+                Assert.That(tookRole, Is.EqualTo(canTakeRole));
+
+                if (!tookRole)
+                    continue;
+
+                Assert.That(ServerSession!.AttachedEntity, Is.Not.Null);
+
+                // Ensure we spawned in the correct location
+                var sessionXform = SEntMan.GetComponent<TransformComponent>(ServerSession.AttachedEntity.Value);
+                Assert.That(sessionXform.MapUid, Is.EqualTo(xform.MapUid));
+
+                // We break it up like this cause otherwise it'll sometimes randomly fail
+                // TODO: Engine IEquatable for EntityCoordinates
+                Assert.That(sessionXform.Coordinates.EntityId, Is.EqualTo(xform.Coordinates.EntityId));
+
+                // I will not get heisentest due to floating point errors
+                Assert.That(MathHelper.CloseTo(sessionXform.Coordinates.X, xform.Coordinates.X, 0.001f), Is.True);
+                Assert.That(MathHelper.CloseTo(sessionXform.Coordinates.Y, xform.Coordinates.Y, 0.001f), Is.True);
+            }
+
+            // Ensure all ghost roles spawned and were assigned!!!
+            Assert.That(rules.Values, Is.All.Zero);
+
+            // End all rules
+            _ticker.ClearGameRules();
+            Assert.That(_ticker.GetAddedGameRules(), Is.Empty);
         }
-
-        var roleEnumerator = SEntMan.EntityQueryEnumerator<GhostRoleAntagSpawnerComponent, GhostRoleComponent, TransformComponent>();
-        while (roleEnumerator.MoveNext(out var spawner, out var role, out var xform))
+        finally
         {
-            // Ensure the ghost role spawner spawned correctly!
-            Assert.That(spawner.Rule, Is.EqualTo(gameRule));
-            Assert.That(spawner.Definition, Is.Not.Null);
-            Assert.That(xform.MapUid, Is.Not.Null);
-            Assert.That(xform.MapID, Is.Not.EqualTo(MapId.Nullspace));
-
-            var value = rules[spawner.Definition.Value];
-            rules[spawner.Definition.Value] = value - 1;
-
-            // Take the ghost role and ensure we take it!
-            Assert.That(_ghostRole.Takeover(ServerSession!, role.Identifier), Is.True);
-            Assert.That(ServerSession!.AttachedEntity, Is.Not.Null);
-
-            // Ensure we spawned in the correct location
-            var sessionXform = SEntMan.GetComponent<TransformComponent>(ServerSession.AttachedEntity.Value);
-            Assert.That(sessionXform.MapUid, Is.EqualTo(xform.MapUid));
-
-            // We break it up like this cause otherwise it'll sometimes randomly fail
-            // TODO: Engine IEquatable for EntityCoordinates
-            Assert.That(sessionXform.Coordinates.EntityId, Is.EqualTo(xform.Coordinates.EntityId));
-
-            // I will not get heisentest due to floating point errors
-            Assert.That(MathHelper.CloseTo(sessionXform.Coordinates.X, xform.Coordinates.X, 0.001f), Is.True);
-            Assert.That(MathHelper.CloseTo(sessionXform.Coordinates.Y, xform.Coordinates.Y, 0.001f), Is.True);
+            serverCfg.SetCVar(StarlightCCVars.DisableLoadMapRule, true);
         }
-
-        // Ensure all ghost roles spawned and were assigned!!!
-        Assert.That(rules.Values, Is.All.Zero);
-
-        // End all rules
-        _ticker.ClearGameRules();
-        Assert.That(_ticker.GetAddedGameRules(), Is.Empty);
+        #endregion
     }
 }
