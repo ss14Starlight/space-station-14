4ba0a1b5724230090090a8aa970f4797e7cecf3c
Arkanic <50847107+Arkanic@users.noreply.github.com>
2026-07-16 23:16:28 +1200

    Massively reduce AI lag (#5117)
    ## Short description
    Quick description bc im in a rush 
    AI visual system in testing, net total 3 second cpu use -> 20ms
    
    Red mushie requested I take a look at this system because it's been
    super laggy for a while, up to 4ms on alpha
    
    As it turns out, the AI vision system runs a super expensive "is
    accessible" check when you have a bui open, except this check, for some
    unholy reason, runs once per bui, per ai, per tick, despite it being
    global to all ai
    
    anyway what it means is that if ai has a ui open they literally cause
    the server to shit itself
    
    given that the occlusion check is the same result for all AI, is
    incredibly expensive, and is something that doesn't change frequently
    (how many times per shift is an apc going to come in or go out of view
    of AI, maybe like once due to power issues lol), this pr caches the
    result
    
    to test, 30 seconds running around as AI with ui open
    before change: 3 seconds of cpu net used
    after change: 20ms seconds of cpu used
    
    genuinely insane
    
    ## Why we need to add this
    guys pls help alfa is laggy!!1
    
    
    ## Checks
    <!-- check boxes for faster reviewing of your PR -->
    
    - [x] I do not require assistance to complete the PR.
    - [x] Before posting/requesting review of a PR, I have verified that the
    changes work.
    - [x] I have added screenshots/videos of the changes, or this PR does
    not change in-game mechanics.
    - [x] I affirm that my changes are licensed under the [MIT
    License](https://github.com/ss14Starlight/space-station-14/blob/Starlight/LICENSE.TXT)
    and grant permission for use in this repository under its conditions.
    
    **Changelog**
    
    :cl: Arkanic
    - fix: Massive reduction in serverside AI lag, something like 150x time
    reduction in certain scenarios.

--- Content.Shared/_Starlight/Silicons/Borgs/StationAIShuntSystem.cs
+++ Content.Shared/_Starlight/Silicons/Borgs/StationAIShuntSystem.cs
@@ -50,7 +50,7 @@ public sealed partial class StationAIShuntSystem : EntitySystem
         if (ev.Handled)
             return;
         var target = ev.Target;
-        if (_vision.IsOutsideCameraView(target))
+        if (_vision.IsOutsideCameraViewCached(target))
             return;
 
         // If target has ShuntThrough component, search for a valid target in containers
