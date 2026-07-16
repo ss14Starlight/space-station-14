11c3784747c68273ac0314b7c7d4d8d9894c463d
JohnnyMalawny <johnnymalawny@gmail.com>
2026-07-16 01:09:51 -0700

    caws stuffs

--- Content.IntegrationTests/Tests/_Starlight/Power/GridPowerTests.cs
+++ Content.IntegrationTests/Tests/_Starlight/Power/GridPowerTests.cs
@@ -79,6 +79,7 @@ public sealed class GridPowerTests : GameTest
         new("/Maps/_Starlight/Shuttles/Munchies.yml"),
         new("/Maps/_Starlight/Shuttles/Mini_Ingeniator.yml"),
         new("/Maps/_Starlight/Shuttles/Comet.yml"),
+        new("/Maps/_Starlight/Shuttles/GasTransport.yml"),
 
         // Syndicate
         new("/Maps/_Starlight/Shuttles/blackhorse.yml"),
