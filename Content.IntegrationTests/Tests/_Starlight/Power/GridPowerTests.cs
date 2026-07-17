b34b52896c0ec665c8aab4c4d60d5253ee0306df
Infiniteb1u3 <135785843+Infiniteb1u3@users.noreply.github.com>
2026-07-17 21:55:00 +1000

    Adds a new Chem/botany shuttle to shipyard console, priced at 40k Spesos, 18x11 in size
    Why we need to add this:
    Adding a new shuttle to the Shipyard inventory and allowing chemists or botany players to focus on producing chems/plants that they are primarily interested in directly

--- Content.IntegrationTests/Tests/_Starlight/Power/GridPowerTests.cs
+++ Content.IntegrationTests/Tests/_Starlight/Power/GridPowerTests.cs
@@ -80,6 +80,7 @@ public sealed class GridPowerTests : GameTest
         new("/Maps/_Starlight/Shuttles/Munchies.yml"),
         new("/Maps/_Starlight/Shuttles/Mini_Ingeniator.yml"),
         new("/Maps/_Starlight/Shuttles/Comet.yml"),
+        new("/Maps/_Starlight/Shuttles/JSS_MED_Apotherkerin.yml"),
 
         // Syndicate
         new("/Maps/_Starlight/Shuttles/blackhorse.yml"),
