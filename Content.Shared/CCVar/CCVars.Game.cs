89d2e6cf7dd1fafa6f72c85e29db0f763c14af66
Zenpanku-sys <86247868+Zenpanku-sys@users.noreply.github.com>
2026-07-17 03:40:47 +0100

    Update Reach - WEH! (#31)
    ## Short description
    <!-- What do you propose to change with your PR? -->
    Update Reach so engineering & cargo is actually "good".
    ## Why we need to add this
    <!-- What is the reason for adding these changes? Please post links to
    Discussions as well as Bug Reports here. Please describe how this will
    change the game balance. -->
    Reach QoL -🔥
    ## Media (Video/Screenshots)
    <!--
    If your PR contains in-game changes you must provide screenshots/videos
    of the changes.
    -->
    N/A
    ## Checks
    <!-- check boxes for faster reviewing of your PR -->
    
    - [X] I do not require assistance to complete the PR.
    - [x] Before posting/requesting review of a PR, I have verified that the
    changes work.
    - [X] I have added screenshots/videos of the changes, or this PR does
    not change in-game mechanics.
    - [x] I affirm that my changes are licensed under the [MIT
    License](https://github.com/North-Western-Development/space-station-14/blob/Sol/LICENSE.TXT)
    and grant permission for use in this repository under its conditions.
    
    **Changelog**
    :cl: Beans
    - tweak: Updated Reach - Engi / Cargo.
    
    ---------
    
    Co-authored-by: ChillGreenBean <ChillGreenBean@gmail.com>
    Co-authored-by: Jackson Abney <44131234+AprilCatStreams@users.noreply.github.com>

--- Content.Shared/CCVar/CCVars.Game.cs
+++ Content.Shared/CCVar/CCVars.Game.cs
@@ -88,7 +88,7 @@ public sealed partial class CCVars
         ///     Prototype to use for map pool.
         /// </summary>
         public static readonly CVarDef<string>
-            GameMapPool = CVarDef.Create("game.map_pool", "DefaultStarlightMapPool", CVar.SERVERONLY);
+            GameMapPool = CVarDef.Create("game.map_pool", "DefaultSolMapPool", CVar.SERVERONLY);
 
     /// <summary>
     ///     The depth of the queue used to calculate which map is next in rotation.
