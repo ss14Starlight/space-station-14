8ed3a7e1b335dd2dbb871af8e0fe8086d7a834b6
wonderfulnewworld <shinsekaihibiki@gmail.com>
2026-07-18 02:32:41 -0700

    fuck. terror. spiders.

--- Content.IntegrationTests/Tests/GameRules/AllGamePresetsStartTest.cs
+++ Content.IntegrationTests/Tests/GameRules/AllGamePresetsStartTest.cs
@@ -23,7 +23,7 @@ public sealed class AllGamePresetsStartTest : AntagTest
     /// A list of blacklisted <see cref="GamePresetPrototype"/> for this test. Some down streams might make changes which nuke upstream game modes they don't use.
     /// This prevents them from being tested. If you use this to silence valid test fails and your game fails to start. Skill issue. Do 100 push-ups.
     /// </summary>
-    private static readonly HashSet<string> IgnoredPresets = []; // Is a string to prevent YAML Linter from freaking if this is empty.
+    private static readonly HashSet<string> IgnoredPresets = ["TerrorSpiders"]; // Is a string to prevent YAML Linter from freaking if this is empty.
 
     private static string[] _gamePresets = GameDataScrounger.PrototypesOfKind<GamePresetPrototype>().Where(p => !IgnoredPresets.Contains(p)).ToArray();
 
