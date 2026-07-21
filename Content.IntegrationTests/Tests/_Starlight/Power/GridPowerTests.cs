0251d0a31685b90735773b5c2c703c27eced962d
Dennisz125 <denzabluda@gmail.com>
2026-07-21 01:08:08 -0700

    New Shuttle: NTS "Honeybee" Mini-Flores (#5035)
    ## Short description
    <!-- What do you propose to change with your PR? -->
    Adding a new small Botany Shuttle, NTS "Honeybee" Mini-Flores, to the
    Shipyard Console. Pricing for 30K Spesos. Current size: 16x13 tiles.
    
    ## Why we need to add this
    <!-- What is the reason for adding these changes? Please post links to
    Discussions as well as Bug Reports here. Please describe how this will
    change the game balance. -->
    Adding a new shuttle to the Shipyard inventory and allowing an advanced
    Botanist to use the shuttle if the station's botany setup is subpar
    compared to their standard. It's also a cheaper version of the Bumblee
    Shuttle.
    
    ## Media (Video/Screenshots)
    <!--
    If your PR contains in-game changes you must provide screenshots/videos
    of the changes.
    -->
    <img width="698" height="856" alt="image"
    src="https://github.com/user-attachments/assets/70d1472a-2fbb-42d8-93ee-a685f9c3ce7a"
    />
    <img width="698" height="859" alt="image"
    src="https://github.com/user-attachments/assets/ac961f7c-ce95-466e-b237-cb69510b0d77"
    />
    
    
    ## Checks
    <!-- check boxes for faster reviewing of your PR -->
    
    - [x] I do not require assistance to complete the PR.
    - [x] Before posting/requesting review of a PR, I have verified that the
    changes work.
    - [x] I have added screenshots/videos of the changes, or this PR does
    not change in-game mechanics.
    - [x] I affirm that my changes are licensed under the [MIT
    License](https://github.com/ss14Starlight/space-station-14/blob/Starlight/LICENSE.TXT)
    and grant permission for use in this repository under its conditions.
    
    **Changelog**
    <!--
    If you want the players to know about changes made in this PR, specify
    them using the template outside the comment. Short and informative.
    
    :cl: STARLIGHT TEAM
    - add: Added Starlight.
    - remove: Removed SS13.
    - tweak: Changed SS14.
    - fix: Fixed Rinary.
    -->
    :cl: Hot_Mic
    add: Added Shuttle "NTS "Honeybee" Mini-Flores", a purchasable botany
    shuttle in the shipyard console for 30,000 Spesos.
    
    ---------
    
    Co-authored-by: CawsForConcern <kopczynski.zesty@gmail.com>
    Co-authored-by: Red Mushie <82113471+redmushie@users.noreply.github.com>

--- Content.IntegrationTests/Tests/_Starlight/Power/GridPowerTests.cs
+++ Content.IntegrationTests/Tests/_Starlight/Power/GridPowerTests.cs
@@ -81,6 +81,7 @@ public sealed class GridPowerTests : GameTest
         new("/Maps/_Starlight/Shuttles/Mini_Ingeniator.yml"),
         new("/Maps/_Starlight/Shuttles/Bumblebee.yml"),
         new("/Maps/_Starlight/Shuttles/Comet.yml"),
+        new("/Maps/_Starlight/Shuttles/Honeybee.yml"),
 
         // Syndicate
         new("/Maps/_Starlight/Shuttles/blackhorse.yml"),
