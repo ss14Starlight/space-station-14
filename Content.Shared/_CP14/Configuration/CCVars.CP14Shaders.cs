e15c7ddfb5a14f316cc3c8077111cc16ab17fd3b
wonderfulnewworld <shinsekaihibiki@gmail.com>
2026-07-25 01:57:46 -0700

    the third pr

--- Content.Shared/_CP14/Configuration/CCVars.CP14Shaders.cs
+++ Content.Shared/_CP14/Configuration/CCVars.CP14Shaders.cs
@@ -1,19 +1,15 @@
 using Robust.Shared;
 using Robust.Shared.Configuration;
 
-namespace Content.Shared._CP14.Configuration;
+namespace Content.Shared.CCVar;
 
-[CVarDefs]
-public sealed class CP14ConfigVars : CVars
+public sealed partial class CCVars
 {
-    public static readonly CVarDef<bool>
-        WaveShaderEnabled = CVarDef.Create("cp14_rendering.wave_shader_enabled", true, CVar.CLIENT | CVar.ARCHIVE);
-
     /// <summary>
     /// Toggle for non-gameplay-affecting or otherwise status indicative post-process effects, such additive lighting.
     /// In the future, this could probably be turned into an enum that allows only disabling more expensive post-process shaders.
     /// However, for now (mid-July of 2024), this only applies specifically to a particularly cheap shader: additive lighting.
     /// </summary>
     public static readonly CVarDef<bool>
-        PostProcess = CVarDef.Create("cp14_graphics.post_process", true, CVar.CLIENTONLY | CVar.ARCHIVE);
+        PostProcess = CVarDef.Create("cp14_shaders.post_process", true, CVar.CLIENTONLY | CVar.ARCHIVE);
 }
