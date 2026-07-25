b1279b03ddcb14d6a92e80418bf1bd2dff5c7873
Rinary <rinary.super@gmail.com>
2026-07-25 23:07:13 +0300

    fix

--- Content.Server/Administration/Commands/PardonCommand.cs
+++ Content.Server/Administration/Commands/PardonCommand.cs
@@ -1,4 +1,5 @@
-﻿using Content.Server.Administration.Managers;
+﻿using System.Threading.Tasks;
+using Content.Server.Administration.Managers;
 using Content.Server.Database;
 using Content.Shared.Administration;
 using Robust.Shared.Console;
@@ -14,6 +15,21 @@ namespace Content.Server.Administration.Commands
         public override string Command => "pardon";
 
         public override async void Execute(IConsoleShell shell, string argStr, string[] args)
+        {
+            // Starlight-start: Move all code into internal method and use catch to catch errors
+            try
+            {
+                await ExecuteInternal(shell, argStr, args);
+            }
+            catch (Exception e)
+            {
+                shell.WriteError($"Pardon failed: {e.Message}");
+                Logger.GetSawmill("admin.bans").Error($"Pardon command failed: {e}");
+            }
+            // Starlight-end
+        }
+
+        public async Task ExecuteInternal(IConsoleShell shell, string argStr, string[] args) // Starlight-edit
         {
             var player = shell.Player;
 
@@ -56,7 +72,7 @@ namespace Content.Server.Administration.Commands
                 return;
             }
 
-            await _banManager.CreateServerUnban(banId, player?.UserId, DateTimeOffset.Now); // NullLink-edit: move to general method at Manager
+            await _banManager.CreateServerUnban(banId, player?.UserId, DateTimeOffset.Now, ban.ProjectName, ban.ServerName); // NullLink-edit: move to general method at Manager
 
             shell.WriteLine(Loc.GetString($"cmd-pardon-success", ("id", banId)));
         }
