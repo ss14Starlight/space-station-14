5a73e9a4d82bbd3d9c9483878f8faa05b1aa8c7a
RedSpeeds <lars.dormans@live.nl>
2026-07-28 14:29:10 +0200

    Mis typed bracket

--- Content.Server/Shuttles/Systems/EmergencyShuttleSystem.Console.cs
+++ Content.Server/Shuttles/Systems/EmergencyShuttleSystem.Console.cs
@@ -281,21 +281,19 @@ public sealed partial class EmergencyShuttleSystem
             //Starlight edit start - Add ERT shuttles to whitelist on beacon
             // Guarantees that emergency shuttle arrives first before anyone else can FTL.
             while (query.MoveNext(out var comp, out _))
+            {
                 if (Deleted(comp.Entity) || !comp.MapEntity.HasValue || Deleted(comp.MapEntity))
                     continue;
 
                 var mapId = Transform(comp.MapEntity.Value).MapID;
                 if (_shuttle.TryAddFTLDestination(mapId, true, false, false, out var ftlDestinationComponent))
                 {
-                    var whitelistInst = new EntityWhitelist
-                    {
-                        Tags = new() { "ERTShuttle" }
-                    };
+                    var whitelistInst = new EntityWhitelist { Tags = new() { "ERTShuttle" } };
                     _shuttle.SetFTLWhitelist((comp.MapEntity.Value, ftlDestinationComponent), whitelistInst);
                 }
             }
-            //Starlight-edit end
         }
+            //Starlight-edit end
     }
 
     private void OnEmergencyRepealAll(EntityUid uid, EmergencyShuttleConsoleComponent component, EmergencyShuttleRepealAllMessage args)
