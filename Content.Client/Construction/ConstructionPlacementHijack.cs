53d7b0da2d72dcefe39feb29402376c77af4743d
Red Mushie <noreply@redmushie.me>
2026-07-31 14:31:28 +0200

    Fix construction ghost rendering

--- Content.Client/Construction/ConstructionPlacementHijack.cs
+++ Content.Client/Construction/ConstructionPlacementHijack.cs
@@ -58,6 +58,39 @@ namespace Content.Client.Construction
                 return;
 
             manager.CurrentTextures = SpriteComponent.GetPrototypeTextures(proto, IoCManager.Resolve<IResourceCache>()).ToList();
+
+            // Starlight BEGIN
+
+            // Make the whole thing transparent just like a placed ghost. It's impossible to see e.g. pipes if they're
+            // obscured by the main sprite body.
+            if (manager.CurrentMode is { } mode)
+            {
+                mode.ValidPlaceColor = mode.ValidPlaceColor.WithAlpha(0.5f);
+                mode.InvalidPlaceColor = mode.InvalidPlaceColor.WithAlpha(0.5f);
+            }
+
+            // Starlight: Fix per-layer/sprite Offset being lost by manually applying it to the ghost layers directly.
+            var entMan = IoCManager.Resolve<IEntityManager>();
+            if (proto.TryGetComponent<SpriteComponent>(out var sprite, entMan.ComponentFactory)
+                && manager.CurrentPlacementOverlayEntity is { } overlay
+                && entMan.TryGetComponent<SpriteComponent>(overlay, out var overlaySprite))
+            {
+                var sprites = entMan.System<SpriteSystem>();
+                var i = 0;
+
+                foreach (var layer in sprite.AllLayers)
+                {
+                    if (layer.ActualRsi?.Path == null || layer.RsiState.Name == null)
+                        continue;
+
+                    sprites.LayerSetOffset((overlay, overlaySprite), i, sprite.Offset + ((SpriteComponent.Layer)layer).Offset);
+                    i++;
+                }
+
+                // Starlight: Fix NoRotation also being forgotten.
+                overlaySprite.NoRotation = sprite.NoRotation;
+            }
+            // Starlight END
         }
     }
 }
