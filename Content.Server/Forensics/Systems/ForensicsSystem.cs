93869eb23a6f0766d9a4498ece412d805a433693
Sparlight <twiksparlight@gmail.com>
2026-07-31 00:03:11 -0600

    code review

--- Content.Server/Forensics/Systems/ForensicsSystem.cs
+++ Content.Server/Forensics/Systems/ForensicsSystem.cs
@@ -21,6 +21,7 @@ using Content.Shared.Verbs;
 using Robust.Shared.Utility;
 using Content.Shared.Hands.Components;
 using Content.Server._Starlight.Medical.Body.Systems;
+using Content.Server._Starlight.Scent.Components;
 
 namespace Content.Server.Forensics
 {
@@ -186,7 +187,7 @@ namespace Content.Server.Forensics
         private void OnUtilityVerb(Entity<CleansForensicsComponent> entity, ref GetVerbsEvent<UtilityVerb> args)
         {
             if (!args.CanInteract || !args.CanAccess
-                || HasComp<_Starlight.Scent.Components.CleansScentComponent>(entity.Owner)) // Starlight - prevent scent system conflict
+                || HasComp<CleansScentComponent>(entity.Owner)) // Starlight - prevent scent system conflict
                 return;
 
             // These need to be set outside for the anonymous method!
