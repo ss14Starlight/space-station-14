5ef0fe66417b51e5d37cfa5b616d85597c043186
VBlankFF <vblankff@gmail.com>
2026-08-01 08:14:53 -0400

    Added a component and system for adding the accesses instead, so the accesses stack. Also, it now adds DoorBumpOpener

---
+++ Content.Server/Xenoarchaeology/Artifact/XAE/Components/XAEGainAccessComponent.cs
@@ -0,0 +1,17 @@
+﻿using Content.Shared.Access;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.Xenoarchaeology.Artifact.XAE.Components;
+
+/// <summary>
+/// XenoArtifact effect that grant accesses to artifacts.
+/// </summary>
+[RegisterComponent, Access(typeof(XAEGainAccessSystem))]
+public sealed partial class XAEGainAccessComponent : Component
+{
+    /// <summary>
+    /// The accesses the artifact will have.
+    /// </summary>
+    [DataField] public HashSet<ProtoId<AccessLevelPrototype>> Accesses = new();
+}
+
---
+++ Content.Server/Xenoarchaeology/Artifact/XAE/XAEGainAccessSystem.cs
@@ -0,0 +1,43 @@
+﻿using Content.Server.Xenoarchaeology.Artifact.XAE.Components;
+using Content.Shared.Xenoarchaeology.Artifact.XAE;
+using Content.Shared.Xenoarchaeology.Artifact;
+using Content.Shared.Access.Components;
+using Content.Shared.Tag;
+using Content.Shared.Trigger.Components.Effects;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.Xenoarchaeology.Artifact.XAE;
+
+/// <summary>
+/// System for xeno artifact activation effect that gives artifacts accesses.
+/// </summary>
+public sealed partial class XAEGainAccessSystem : BaseXAESystem<XAEGainAccessComponent>
+{
+    [Dependency] private TagSystem _tag = default!;
+    /// <inheritdoc />
+    protected override void OnActivated(Entity<XAEGainAccessComponent> ent, ref XenoArtifactNodeActivatedEvent args)
+    {
+        // Give the artifact an AccessComponent if it lacks one, else get the existing AccessComponent
+        if (!TryComp(args.Artifact, out AccessComponent? component))
+        {
+            component = EntityManager.ComponentFactory.GetComponent<AccessComponent>();
+            AddComp(args.Artifact, component);
+        }
+
+        var beforeLength = component.Tags.Count;
+
+        // Adds the new accesses to the artifact
+        foreach (var access in ent.Comp.Accesses)
+        {
+            component.Tags.Add(access);
+        }
+
+        // Is there no better way to do this?
+        _tag.AddTag(args.Artifact, new ProtoId<TagPrototype>("DoorBumpOpener"));
+
+        if (beforeLength != component.Tags.Count)
+        {
+            Dirty(args.Artifact, component);
+        }
+    }
+}
