565510bbac6fda9b53f1ea89097b484f7ef5f6db
SirLutz <zockerlutz1024@gmail.com>
2026-08-02 18:39:17 +0200

    shard rerun

--- Content.Shared/_Starlight/CosmicCult/Components/CosmicCultFactionComponent.cs
+++ Content.Shared/_Starlight/CosmicCult/Components/CosmicCultFactionComponent.cs
@@ -3,7 +3,6 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._Starlight.CosmicCult.Components;
 /// <summary>
 /// Marker component for Cosmic Cult-aligned entities.
-///
 /// Used to identify hostile Cosmic entities separately from player cultists,
 /// allowing systems such as faction icon visibility to recognize them as Cosmic Cult-affiliated
 /// </summary>
