be4564bd1418be6d463d2c1f0e61c4817d9b2fe2
Sparlight <twiksparlight@gmail.com>
2026-08-03 12:42:23 -0600

    Lose scent tracking when dead

--- Content.Shared/_Starlight/Scent/Systems/SharedScentSystem.cs
+++ Content.Shared/_Starlight/Scent/Systems/SharedScentSystem.cs
@@ -1,6 +1,7 @@
 using Content.Shared._Starlight.Scent.Components;
 using Content.Shared.Actions;
 using Content.Shared.Actions.Components;
+using Content.Shared.Mobs;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Popups;
 using Robust.Shared.Audio.Systems;
@@ -23,6 +24,13 @@ public abstract class SharedScentSystem : EntitySystem
         SubscribeLocalEvent<SmellerComponent, ComponentShutdown>(OnSmellerShutdown);
         SubscribeLocalEvent<SmellerComponent, ToggleSniffActionEvent>(OnToggleSniff);
         SubscribeLocalEvent<SmellerComponent, SneezeActionEvent>(OnSneeze);
+        SubscribeLocalEvent<SmellerComponent, MobStateChangedEvent>(OnMobStateChanged);
+    }
+
+    private void OnMobStateChanged(Entity<SmellerComponent> ent, ref MobStateChangedEvent args)
+    {
+        if (args.NewMobState == Content.Shared.Mobs.MobState.Dead)
+            ClearTrackedScent(ent);
     }
 
     private void OnSmellerMapInit(Entity<SmellerComponent> ent, ref MapInitEvent args)
