dd51482d8c1dcbf3e7b70cc3f790530f96f0be8d
Xale <73114897+XaleMCY@users.noreply.github.com>
2026-08-03 20:21:51 +0200

    Fix UpdateCuffState to handle HandsComponent == null

--- Content.Shared/Cuffs/SharedCuffableSystem.cs
+++ Content.Shared/Cuffs/SharedCuffableSystem.cs
@@ -175,7 +175,7 @@ namespace Content.Shared.Cuffs
 
         public void UpdateCuffState(EntityUid uid, CuffableComponent component)
         {
-            var canInteract = TryComp(uid, out HandsComponent? hands) && hands.Hands.Count > component.CuffedHandCount;
+            var canInteract = !TryComp(uid, out HandsComponent? hands) || hands.Hands.Count > component.CuffedHandCount;
 
             if (canInteract == component.CanStillInteract)
                 return;
