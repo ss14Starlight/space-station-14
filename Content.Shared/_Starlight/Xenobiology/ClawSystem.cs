3bcce25d7b30d108051b226bcacf9357aabbaa8c
Jackson Huffstutler <jack.huffstutler@gmail.com>
2026-08-03 23:38:00 -0500

    Claw now pauses whatever is picked up

--- Content.Shared/_Starlight/Xenobiology/ClawSystem.cs
+++ Content.Shared/_Starlight/Xenobiology/ClawSystem.cs
@@ -8,18 +8,19 @@ namespace Content.Shared._Starlight.Xenobiology;
 
 public sealed class ClawSystem : EntitySystem
 {
-    [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
-    [Dependency] private readonly SharedInteractionSystem _interactionSystem = default!;
-    [Dependency] private readonly TagSystem _tagSystem = default!;
-    [Dependency] private readonly EntityWhitelistSystem _whitelist = default!;
-    
+    [Dependency] private SharedContainerSystem _containerSystem = default!;
+    [Dependency] private SharedInteractionSystem _interactionSystem = default!;
+    [Dependency] private TagSystem _tagSystem = default!;
+    [Dependency] private EntityWhitelistSystem _whitelist = default!;
+    [Dependency] private MetaDataSystem _metaDataSystem = default!;
+
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<ClawComponent, MapInitEvent>(OnMapInit);
         SubscribeLocalEvent<ClawComponent, AfterInteractEvent>(OnAfterInteract);
     }
-    
+
     private void OnMapInit(Entity<ClawComponent> entity, ref MapInitEvent args)
     {
         entity.Comp.Container = _containerSystem.EnsureContainer<ContainerSlot>(entity.Owner, entity.Comp.ClawContainerId);
@@ -36,12 +37,15 @@ public sealed class ClawSystem : EntitySystem
             if (!canReach) return;
             if (_whitelist.IsWhitelistFail(entity.Comp.AllowedEntities, args.Target.Value)) return;
             _containerSystem.Insert(args.Target.Value, entity.Comp.Container);
+            _metaDataSystem.SetEntityPaused(args.Target.Value, true);
         }
         else
         {
             var canReach = _interactionSystem.InRangeUnobstructed(args.User, args.ClickLocation, entity.Comp.ClawInteractionRange, CollisionGroup.None);
             if (!canReach) return;
-            _containerSystem.Remove(entity.Comp.Container.ContainedEntities[0], entity.Comp.Container, true, false,
+            var ent = entity.Comp.Container.ContainedEntities[0];
+            _metaDataSystem.SetEntityPaused(ent, false);
+            _containerSystem.Remove(ent, entity.Comp.Container, true, false,
                 args.ClickLocation);
         }
     }
