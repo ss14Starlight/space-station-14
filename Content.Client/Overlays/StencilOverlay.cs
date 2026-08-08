32abe30988383dd2b2af4b46338c3fcc5b823c31
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2026-08-08 16:29:35 +1000

    Ambient occlusion overlay optimisation + art change + cleanup (#44262)

--- Content.Client/Overlays/StencilOverlay.cs
+++ Content.Client/Overlays/StencilOverlay.cs
@@ -1,5 +1,6 @@
 using System.Numerics;
 using Content.Client.Graphics;
+using Content.Client.Light.EntitySystems;
 using Content.Client.Parallax;
 using Content.Client.Weather;
 using Content.Shared.Salvage;
@@ -33,6 +34,7 @@ public sealed partial class StencilOverlay : Overlay
     private readonly SpriteSystem _sprite;
     private readonly WeatherSystem _weather;
     private readonly StatusEffectsSystem _statusEffects;
+    private GridStencilSystem _gridStencil = default!;
     private HashSet<Entity<WeatherStatusEffectComponent, StatusEffectComponent>>? _weatherSet = new();
 
     public override OverlaySpace Space => OverlaySpace.WorldSpaceBelowFOV;
@@ -51,6 +53,7 @@ public sealed partial class StencilOverlay : Overlay
         _weather = weather;
         _statusEffects = statusEffects;
         IoCManager.InjectDependencies(this);
+        _gridStencil = _entManager.System<GridStencilSystem>();
         _shader = _protoManager.Index(CircleShader).InstanceUnique();
     }
 
@@ -68,7 +71,7 @@ public sealed partial class StencilOverlay : Overlay
         }
 
         if (_statusEffects.TryEffectsWithComp(mapUid, out _weatherSet))
-            DrawWeather(args, res, _weatherSet, invMatrix);
+            DrawWeather(args, _weatherSet);
 
         if (_entManager.TryGetComponent<RestrictedRangeComponent>(mapUid, out var restrictedRangeComponent))
             DrawRestrictedRange(args, res, restrictedRangeComponent, invMatrix);
