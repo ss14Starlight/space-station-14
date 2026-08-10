449454d99435b863892436075780104e98e8782f
CawsForConcern <kopczynski.zesty@gmail.com>
2026-08-10 05:05:34 -0700

    Bigass shuttle clean up (#5379)
    ## Short description
    <!-- What do you propose to change with your PR? -->
    In the spirit of #2525, I have gone through and removed a bunch of
    upstream shuttles that are completely unused. Some of these are for
    GameRules that have long since been removed, some of these were shuttles
    related to upstream maps that were removed.
    
    All the same, they're loitering in the files and doing nothing but
    increasing our filesize by a small but non-zero amount and that sucks.
    
    This PR also organizes the shuttle folder by adding the following
    folders:
    - Admeme
    - Cargo
    - Evac
    - Nukeops
    - Shipyard
    
    No longer any of us have to go "what the hell is `pts.yml`" for now you
    can just look at it and go "it must be some Shipyard thing because it's
    in the shipyard folder.
    
    ## Why we need to add this
    <!-- What is the reason for adding these changes? Please post links to
    Discussions as well as Bug Reports here. Please describe how this will
    change the game balance. -->
    I got mad I couldn't find something.
    
    ## Checks
    <!-- check boxes for faster reviewing of your PR -->
    
    - [x] I do not require assistance to complete the PR.
    - [ ] Before posting/requesting review of a PR, I have verified that the
    changes work.
    - [x] I have added screenshots/videos of the changes, or this PR does
    not change in-game mechanics.
    - [x] I affirm that my changes are licensed under the [MIT
    License](https://github.com/ss14Starlight/space-station-14/blob/Starlight/LICENSE.TXT)
    and grant permission for use in this repository under its conditions.
    
    **Changelog**
    <!--
    If you want the players to know about changes made in this PR, specify
    them using the template outside the comment. Short and informative.
    
    :cl: STARLIGHT TEAM
    - add: Added Starlight.
    - remove: Removed SS13.
    - tweak: Changed SS14.
    - fix: Fixed Rinary.
    -->
    :cl: Caws
    - tweak: Organized the shuttle files for both mapper and admin
    convenience.
    - fix: Xenoborgs now use the custom Starlight Mothership once again.
    - remove: Removed a bunch of old/unused shuttle files.

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -74,15 +74,15 @@ namespace Content.IntegrationTests.Tests
             {"/Maps/Shuttles/ShuttleEvent/instigator.yml", ["ShuttleGunFriendship"]},
             {"/Maps/_Starlight/Stations/Cork.yml", ["RubberStampSyndicate"]}, // Starlight start
             {"/Maps/_Starlight/Shuttles/CC-NT/NTSF_Minos_Battlecruiser.yml", ["ShuttleGunPerforator"]},
-            {"/Maps/_Starlight/Shuttles/RecluseClassSHC.yml", ["RubberStampSyndicate"]},
-            {"/Maps/_Starlight/Shuttles/Signaleer.yml", ["RubberStampSyndicate"]},
+            {"/Maps/_Starlight/Shuttles/Admeme/RecluseClassSHC.yml", ["RubberStampSyndicate"]},
+            {"/Maps/_Starlight/Shuttles/Admeme/Signaleer.yml", ["RubberStampSyndicate"]},
             {"/Maps/_Starlight/Shuttles/ShuttleEvent/montague.yml", ["RubberStampSolgovLaw", "RubberStampSolgovRep", "RubberStampTSF", "RubberStampTSMC"]},
             {"/Maps/_Starlight/Shuttles/ShuttleEvent/syndie_evacpod.yml", ["RubberStampSyndicate"]},
             {"/Maps/_Starlight/Nonstations/nukieplanet.yml", ["RubberStampSyndicate"]},
             {"/Maps/_Starlight/Nonstations/nukiewestern.yml", ["RubberStampSyndicate"]},
             {"/Maps/_Starlight/Nonstations/geigerComplex.yml", ["RubberStampSyndicate"]},
             {"/Maps/_Starlight/Dungeon/syndie.yml", ["RubberStampSyndicate"]},
-            {"/Maps/_Starlight/Shuttles/scarletSHCdefenderFinal.yml", ["RubberStampSyndicate", "TraitorCodePaper"]},
+            {"/Maps/_Starlight/Shuttles/Admeme/scarletSHCdefenderFinal.yml", ["RubberStampSyndicate", "TraitorCodePaper"]},
             {"/Maps/_Starlight/Centcomms/CC_Outpost_SC17.yml", ["BoxFolderCentComEmpty", "BoxFolderCentCom", "RubberStampCentcom", "BoxFolderCentComThreePapers"]},
             {"/Maps/_Starlight/Centcomms/CC_Outpost_G24.yml", ["BoxFolderCentCom", "RubberStampCentcom"]},
             {"/Maps/_Starlight/Centcomms/CC_Outpost_GNT9.yml", ["BoxFolderCentCom", "RubberStampCAD", "RubberStampCCD", "RubberStampCDD", "RubberStampCED", "RubberStampCentcom", "RubberStampCID", "RubberStampCMD", "RubberStampCRD", "RubberStampCSD"]}// Starlight end
@@ -99,7 +99,7 @@ namespace Content.IntegrationTests.Tests
         {
             "/Maps/Shuttles/AdminSpawn/**", // admin gaming
            #region starlight
-            "/Maps/_Starlight/Shuttles/Radiotower.yml", // Command stamps - listening post.
+            "/Maps/_Starlight/Shuttles/Admeme/Radiotower.yml", // Command stamps - listening post.
             #endregion
         };
 
