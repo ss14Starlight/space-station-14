bb2348e3f9c9de69752470a1df759b42e46bd637
Walker Fowlkes <47339836+walksanatora@users.noreply.github.com>
2026-08-11 22:08:20 -0700

    Revert "Revert "let get live""

--- Content.Server/Radio/EntitySystems/HeadsetSystem.cs
+++ Content.Server/Radio/EntitySystems/HeadsetSystem.cs
@@ -6,8 +6,9 @@ using Content.Shared.Radio.EntitySystems;
 using Content.Shared._Starlight.TextToSpeech;
 using Robust.Shared.Network;
 using Robust.Shared.Player;
-using Content.Server.Speech; // Starlight
-using Content.Server._Starlight.Language; // Starlight
+using Content.Server.Speech;
+using Content.Server._Starlight.Language;
+using Content.Shared._Starlight.Clothing;
 
 namespace Content.Server.Radio.EntitySystems;
 
@@ -55,16 +56,23 @@ public sealed partial class HeadsetSystem : SharedHeadsetSystem
 
     private void OnSpeak(EntityUid uid, WearingHeadsetComponent component, EntitySpokeEvent args)
     {
+        // Starlight-start
+        HeadsetLoudModeComponent? loudComp = null;
+        if(TryComp<HeadsetLoudModeComponent>(component.Headset, out var comp) && comp.Active)
+        {
+            loudComp = comp;
+        }
+        // Starlight-end
         if (args.Channel != null
             && TryComp(component.Headset, out EncryptionKeyHolderComponent? keys)
             && keys.Channels.Contains(args.Channel.ID))
         {
-            _radio.SendRadioMessage(uid, args.Message, args.Channel, component.Headset, args.Language); // Starlight-edit: This literally never specified language??? bruh.
+            _radio.SendRadioMessage(uid, args.Message, args.Channel, component.Headset, args.Language, loudComp: loudComp); // Starlight-edit: This literally never specified language??? bruh.
             args.Channel = null; // prevent duplicate messages from other listeners.
         }
         //Starlight begin
         if (args.UsingCustomChannel && args.CustomChannel is not null)
-            _radio.SendCustomRadioMessage(uid, args.Message.Text, args.CustomChannel, component.Headset, args.Language); // Custom channel data is already confirmed to exist on this headset
+            _radio.SendCustomRadioMessage(uid, args.Message.Text, args.CustomChannel, component.Headset, args.Language, loudComp: loudComp); // Custom channel data is already confirmed to exist on this headset
         //Starlight end
     }
 
