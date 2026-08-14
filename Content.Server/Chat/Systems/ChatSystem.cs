da8f521c1702fd75f3b4e4cc77277c117404ea1c
RedSpeeds <lars.dormans@live.nl>
2026-08-14 16:02:05 +0200

    Small change to make the TTS say the correct chat message instead of one with all abriviations and emote texts

--- Content.Server/Chat/Systems/ChatSystem.cs
+++ Content.Server/Chat/Systems/ChatSystem.cs
@@ -234,6 +234,7 @@ public sealed partial class ChatSystem : SharedChatSystem
             || (CultureInfo.CurrentCulture.IsNeutralCulture && CultureInfo.CurrentCulture.Name == "en");
 
         message.Text = SanitizeInGameICMessage(source, message.Text, out var emoteStr, shouldCapitalize, shouldPunctuate, shouldCapitalizeTheWordI); //Starlight
+        message.Tts = message.Text;
         message.OriginalText = message.Text; // starlight
 
         // Was there an emote in the message? If so, send it.
