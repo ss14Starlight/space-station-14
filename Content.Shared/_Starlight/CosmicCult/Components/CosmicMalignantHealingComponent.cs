a4c93b85cb1167e0501b322791d7a4b175428e07
SirLutz <zockerlutz1024@gmail.com>
2026-08-18 22:40:08 +0200

    XML Docu for malignhealing

--- Content.Shared/_Starlight/CosmicCult/Components/CosmicMalignantHealingComponent.cs
+++ Content.Shared/_Starlight/CosmicCult/Components/CosmicMalignantHealingComponent.cs
@@ -6,12 +6,21 @@ namespace Content.Shared._Starlight.CosmicCult.Components;
 [RegisterComponent]
 public sealed partial class CosmicMalignantHealingComponent : Component
 {
+    /// <summary>
+    /// The tile on which the entity is healed.
+    /// </summary>
     [DataField]
     public ProtoId<ContentTileDefinition> HealingTile = "FloorCosmicCorruption";
 
+    /// <summary>
+    /// The amount of each damage type healed per healing interval.
+    /// </summary>
     [DataField]
     public float HealAmount = 1f;
 
+    /// <summary>
+    /// The time between healing ticks.
+    /// </summary>
     [DataField]
     public TimeSpan HealInterval = TimeSpan.FromSeconds(1);
 
