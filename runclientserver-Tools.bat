f2525be88e86136413fa8f6d1d95a5d7f9a55991
walksanatora <walkerffo22@gmail.com>
2026-08-21 10:16:01 -0700

    remove that random batch file

--- runclientserver-Tools.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-dotnet build Content.Client --configuration Tools
-dotnet build Content.Server --configuration Tools
-
-Start "Client" "runclient-Tools.bat"
-Start "Server" "runserver-Tools.bat"
