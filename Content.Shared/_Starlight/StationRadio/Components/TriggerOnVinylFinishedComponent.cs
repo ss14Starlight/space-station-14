4471ccc6905c28c051d09a9994b4bdd1c1d50f78
walksanatora <walkerffo22@gmail.com>
2026-08-21 11:27:17 -0700

    it all works now.

--- Content.Shared/_Starlight/StationRadio/Components/TriggerOnVinylFinishedComponent.cs
+++ Content.Shared/_Starlight/StationRadio/Components/TriggerOnVinylFinishedComponent.cs
@@ -2,5 +2,5 @@ using Content.Shared.Trigger.Components.Triggers;
 
 namespace Content.Shared._Starlight.StationRadio.Components;
 
-[RegisterComponent]
+[RegisterComponent, AutoGenerateComponentState]
 public sealed partial class TriggerOnVinylFinishedComponent : BaseTriggerOnXComponent {}
