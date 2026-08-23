aa88ef140a87a5f87c9bee62360e45f7f8a0984b
RedSpeeds <lars.dormans@live.nl>
2026-08-24 01:13:57 +0200

    Shutup bot!

--- Content.Client/_Starlight/Lathe/LatheLinkingUIController.cs
+++ Content.Client/_Starlight/Lathe/LatheLinkingUIController.cs
@@ -5,6 +5,6 @@ namespace Content.Client._Starlight.Lathe;
 
 public sealed class LatheLinkingUIController : UIController
 {
-    public void SendToggleMessage(EntityUid uid, bool eject) 
-      => EntityManager.RaisePredictiveEvent(new LatheLinkingToggleEvent(EntityManager.GetNetEntity(uid), eject));
+    public void SendToggleMessage(EntityUid uid, bool eject)
+        => EntityManager.RaisePredictiveEvent(new LatheLinkingToggleEvent(EntityManager.GetNetEntity(uid), eject));
 }
