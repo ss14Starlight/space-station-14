614eb90850bace4047ec10ddf0f1c36c5dfde0cd
Sparlight <twiksparlight@gmail.com>
2026-08-25 16:10:44 -0600

    Revert "Examine text"
    This reverts commit 08a9eb2a8d140a237cae99828b20c539b977d9be.

--- Content.Shared/IdentityManagement/IdentitySystem.cs
+++ Content.Shared/IdentityManagement/IdentitySystem.cs
@@ -57,8 +57,6 @@ public sealed partial class IdentitySystem : EntitySystem
         SubscribeLocalEvent<IdentityComponent, WearerMaskToggledEvent>((uid, _, _) => QueueIdentityUpdate(uid));
         SubscribeLocalEvent<IdentityComponent, EntityRenamedEvent>((uid, _, _) => QueueIdentityUpdate(uid));
         SubscribeLocalEvent<IdentityComponent, VoiceMaskNameUpdatedEvent>((uid, _, _) => QueueIdentityUpdate(uid));
-
-        SubscribeLocalEvent<AnimalIdentityComponent, ExaminedEvent>(OnAnimalExamined); // Starlight
     }
 
     /// <summary>
@@ -210,20 +208,6 @@ public sealed partial class IdentitySystem : EntitySystem
         RaiseLocalEvent(target, ev);
         return representation.ToStringKnown(!ev.Cancelled, ev.NameOverride);
     }
-
-    // Starlight Begin
-    /// <summary>
-    /// Mirrors HumanoidAppearanceSystem's own examine line ("She is a young human.") for
-    /// animal identity holders, minus the age clause ("She is a dog.").
-    /// </summary>
-    private void OnAnimalExamined(EntityUid uid, AnimalIdentityComponent component, ExaminedEvent args)
-    {
-        var identity = Identity.Entity(uid, EntityManager);
-        var noun = Loc.GetString(component.NounId);
-
-        args.PushText(Loc.GetString("animal-identity-component-examine", ("user", identity), ("noun", noun)));
-    }
-    // Starlight End
 
     /// <summary>
     /// Gets an 'identity representation' of an entity, with their true name being the entity name
--- Resources/Locale/en-US/_Starlight/identity/identity-system.ftl
+++ Resources/Locale/en-US/_Starlight/identity/identity-system.ftl
@@ -4,5 +4,3 @@ identity-gender-animal-generic = good { $noun }
 
 identity-animal-noun-dog = dog
 identity-animal-noun-fox = fox
-
-animal-identity-component-examine = { CAPITALIZE(SUBJECT($user)) } { CONJUGATE-BE($user) } { INDEFINITE($noun) } { $noun }.
