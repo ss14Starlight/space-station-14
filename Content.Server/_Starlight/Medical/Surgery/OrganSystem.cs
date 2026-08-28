e246ea5803670cec02f262232b276165cfb68ad5
Citrea_Lingua <citrea.lingua@gmx.net>
2026-08-28 14:09:31 +0200

    readonlyn't data fields

--- Content.Server/_Starlight/Medical/Surgery/OrganSystem.cs
+++ Content.Server/_Starlight/Medical/Surgery/OrganSystem.cs
@@ -34,16 +34,16 @@ namespace Content.Server._Starlight.Medical.Surgery;
 public sealed partial class OrganSystem : EntitySystem
 {
 
-    [Dependency] private readonly BlindableSystem _blindable = default!;
-    [Dependency] private readonly DamageableSystem _damageableSystem = default!;
-    [Dependency] private readonly HumanoidAppearanceSystem _humanoidAppearanceSystem = default!;
-    [Dependency] private readonly TagSystem _tag = default!;
-    [Dependency] private readonly LanguageSystem _language = default!;
-    [Dependency] private readonly SharedContainerSystem _container = default!;
+    [Dependency] private BlindableSystem _blindable = default!;
+    [Dependency] private DamageableSystem _damageableSystem = default!;
+    [Dependency] private HumanoidAppearanceSystem _humanoidAppearanceSystem = default!;
+    [Dependency] private TagSystem _tag = default!;
+    [Dependency] private LanguageSystem _language = default!;
+    [Dependency] private SharedContainerSystem _container = default!;
     [Dependency] private IGameTiming _timing = default!;
-    [Dependency] private readonly MarkingManager _markingManager = default!;
-    [Dependency] private readonly SharedBodySystem _body = default!;
-    [Dependency] private readonly SharedActionsSystem _actionsSystem = default!;
+    [Dependency] private MarkingManager _markingManager = default!;
+    [Dependency] private SharedBodySystem _body = default!;
+    [Dependency] private SharedActionsSystem _actionsSystem = default!;
     [Dependency] private ISerializationManager _serialization = default!;
     [Dependency] private SharedSurgerySystem _surgery = default!;
 
