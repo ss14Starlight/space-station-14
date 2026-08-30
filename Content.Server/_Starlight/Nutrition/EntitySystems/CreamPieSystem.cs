34850ae6ef9b66416f5756207fe13c059075f4e9
wonderfulnewworld <shinsekaihibiki@gmail.com>
2026-08-30 16:11:22 -0700

    gorilla cleanup

--- Content.Server/_Starlight/Nutrition/EntitySystems/CreamPieSystem.cs
+++ Content.Server/_Starlight/Nutrition/EntitySystems/CreamPieSystem.cs
@@ -1,7 +1,5 @@
-#region Starlight
 using Content.Shared.Nutrition.EntitySystems;
 
-namespace Content.Server.Nutrition.EntitySystems;
+namespace Content.Server._Starlight.Nutrition.EntitySystems;
 
 public sealed partial class CreamPieSystem : SharedCreamPieSystem;
-#endregion
