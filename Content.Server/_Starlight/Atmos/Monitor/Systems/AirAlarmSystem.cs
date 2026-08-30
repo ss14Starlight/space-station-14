a14cc089acbaa8d8fb8fcc3f673e5b23a22d7824
walksanatora <walkerffo22@gmail.com>
2026-08-30 16:00:24 -0700

    some more work... but it is stopping early.

--- Content.Server/_Starlight/Atmos/Monitor/Systems/AirAlarmSystem.cs
+++ Content.Server/_Starlight/Atmos/Monitor/Systems/AirAlarmSystem.cs
@@ -6,8 +6,10 @@ using Content.Shared.Maps;
 using Content.Shared.Verbs;
 using Content.Shared.Whitelist;
 using Robust.Server.GameObjects;
+using Robust.Shared.Configuration;
 using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
 
 namespace Content.Server.Atmos.Monitor.Systems;
 
@@ -29,17 +31,17 @@ public sealed partial class AirAlarmSystem
     /// </summary>
     private readonly EntityWhitelist _stoppingWhitelist = new()
     {
-        Tags = [ "Wall" ],
+        Tags = ["Wall"],
         Components = [
             "Firelock"
         ]
     };
-    private readonly Dictionary<int,Vector2i> _offset = new()
+    private readonly Dictionary<int, Vector2i> _offset = new()
     {
-        [0] = new Vector2i(0,-1), //South
-        [1] = new Vector2i(1,0), //East
-        [2] = new Vector2i(0,1), //North
-        [3] = new Vector2i(-1,0), //West
+        [0] = new Vector2i(0, -1), //South
+        [1] = new Vector2i(1, 0), //East
+        [2] = new Vector2i(0, 1), //North
+        [3] = new Vector2i(-1, 0), //West
     };
 
     [Dependency] private TurfSystem _turf = default!;
@@ -49,51 +51,68 @@ public sealed partial class AirAlarmSystem
     //for testing reasons
     private EntProtoId _lizardPlush = "PlushieLizard";
 
+    private int MaxDepth = 10;
     private void SLInitialize()
     {
-        SubscribeLocalEvent<AirAlarmComponent,GetVerbsEvent<Verb>>(OnGetVerbs);
+        SubscribeLocalEvent<AirAlarmComponent, GetVerbsEvent<Verb>>(OnGetVerbs);
+
+
     }
 
-    private void OnGetVerbs(Entity<AirAlarmComponent> ent, ref GetVerbsEvent<Verb> ev)
+    private void OnGetVerbs(Entity<AirAlarmComponent> ent, ref GetVerbsEvent<Verb> ev) => ev.Verbs.Add(new Verb()
     {
-        var xform = Transform(ent);
-        var offset = _offset[(int)(
-            xform.LocalRotation / 90 //make it into a mutiple of 90
-            % 4 //and then normalize to NSEW
-        )];
-
-        if (!_xform.TryGetMapOrGridCoordinates(ent, out var coords, xform))
-            return; //no coords?
-
-        var seen = new HashSet<Vector2i>();
-        var start = coords.Value.ToVector2i(EntityManager, _xform) + offset;
-        var work = new HashSet<Vector2i>
+        Text = Loc.GetString("admin-verb-autolink"),
+        Icon = new SpriteSpecifier.Rsi(new("/Textures/Structures/Wallmounts/air_monitors.rsi"), "alarmp"),
+        Category = VerbCategory.Tricks,
+        Act = () =>
         {
-            start
-        };
-        while (work.Count > 0)
-        {
-            var test = work.First();
-            work.Remove(test);
-            seen.Add(test);
-            //and we do checking here
-            var epos = new EntityCoordinates(ent, test);
-            SpawnAtPosition(_lizardPlush, epos);
+            var xform = Transform(ent);
+            var offset = _offset[(int)(
+               xform.LocalRotation.Degrees / 90 //make it into a mutiple of 90
+               % 4 //and then normalize to NSEW
+            )];
 
-            var stop = _turf.GetEntitiesInTile(epos)
-                .Select( x => _whitelist.IsWhitelistPass(_stoppingWhitelist, x))
-                .Any(x => x);
-            if (stop)
-                continue;
-            foreach (var direction in _offset.Values)
+            if (!_xform.TryGetMapOrGridCoordinates(ent, out var coords, xform))
+                return; //no coords?
+
+            var seen = new HashSet<EntityCoordinates>();
+            var start = coords.Value.Offset(offset);
+            var work = new HashSet<(EntityCoordinates,int)>
             {
-                var newWork = test + direction;
-                if (!seen.Contains(newWork))
-                    work.Add(newWork);
-            }
-        }
-        //xform.LocalRotation
-        //xform.GridUid
+                (start,0)
+            };
+            Log.Info($"starting at {start}");
+            while (work.Count > 0)
+            {
+                var part = work.First();
+                var test = part.Item1;
+                work.Remove(part);
+                seen.Add(test);
+                //and we do checking here
+                Log.Info($"working on {test}, depth {part.Item2} ({work.Count} to process)");
+                SpawnAtPosition(_lizardPlush, test);
 
-    }
+                var tref = _turf.GetTileRef(test);
+                var stop = _turf.GetEntitiesInTile(test)
+                   .Select(x => _whitelist.IsWhitelistPass(_stoppingWhitelist, x))
+                   .Where(x => _turf.GetTileRef(_xform.GetGridTilePositionOrDefault(x)))
+                   .Any(x => x);
+                if (stop || part.Item2 > MaxDepth)
+                {
+                    Log.Info($"Stopping, {stop} depth: {part.Item2}");
+                    continue;
+                }
+                foreach (var direction in _offset.Values)
+                {
+                    var newWork = test.Offset(direction);
+                    if (!seen.Contains(newWork))
+                        work.Add((newWork,part.Item2+2));
+                }
+            }
+            //xform.LocalRotation
+            //xform.GridUid
+        }
+    });
+
+
 }
