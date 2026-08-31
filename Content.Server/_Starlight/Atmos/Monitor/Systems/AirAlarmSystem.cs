f61429eb0e849b51ef63e46cb9eb71aad2b233e6
walksanatora <walkerffo22@gmail.com>
2026-08-30 19:58:24 -0700

    fix the CWE

--- Content.Server/_Starlight/Atmos/Monitor/Systems/AirAlarmSystem.cs
+++ Content.Server/_Starlight/Atmos/Monitor/Systems/AirAlarmSystem.cs
@@ -1,7 +1,9 @@
 //ReSharper disable CheckNamespace
 
 using System.Linq;
+using Content.Server.Administration.Managers;
 using Content.Server.Atmos.Monitor.Components;
+using Content.Shared.Administration;
 using Content.Shared.DeviceNetwork.Components;
 using Content.Shared.Maps;
 using Content.Shared.Verbs;
@@ -9,6 +11,7 @@ using Content.Shared.Whitelist;
 using Robust.Server.GameObjects;
 using Robust.Shared.Configuration;
 using Robust.Shared.Map;
+using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
@@ -47,6 +50,7 @@ public sealed partial class AirAlarmSystem
     [Dependency] private TurfSystem _turf = default!;
     [Dependency] private TransformSystem _xform = default!;
     [Dependency] private EntityWhitelistSystem _whitelist = default!;
+    [Dependency] private IAdminManager _adminManager = default!;
 
 
     //for testing reasons
@@ -58,75 +62,86 @@ public sealed partial class AirAlarmSystem
         SubscribeLocalEvent<AirAlarmComponent, GetVerbsEvent<Verb>>(OnGetVerbs);
     }
 
-    private void OnGetVerbs(Entity<AirAlarmComponent> ent, ref GetVerbsEvent<Verb> ev) => ev.Verbs.Add(new Verb()
+    private void OnGetVerbs(Entity<AirAlarmComponent> ent, ref GetVerbsEvent<Verb> ev)
     {
-        Text = Loc.GetString("admin-verb-autolink"),
-        Icon = new SpriteSpecifier.Rsi(new("/Textures/Structures/Wallmounts/air_monitors.rsi"), "alarmp"),
-        Category = VerbCategory.Tricks,
-        Act = () =>
+        if (!TryComp(ev.User, out ActorComponent? actor))
+            return;
+
+        var player = actor.PlayerSession;
+
+        if (!_adminManager.HasAdminFlag(player, AdminFlags.Mapping))
+            return;
+        ev.Verbs.Add(new Verb()
         {
-            if (!TryComp<DeviceListComponent>(ent, out var devList))
-                return;
-
-            var xform = Transform(ent);
-            var offset = _offset[(int)(
-               xform.LocalRotation.Degrees / 90 //make it into a mutiple of 90
-               % 4 //and then normalize to NSEW
-            )];
-
-            if (!_xform.TryGetMapOrGridCoordinates(ent, out var coords, xform))
-                return; //no coords?
-
-            var seen = new HashSet<EntityCoordinates>();
-            var start = coords.Value.Offset(offset);
-            var work = new HashSet<(EntityCoordinates, int)>
+            Text = Loc.GetString("admin-verb-autolink"),
+            Icon = new SpriteSpecifier.Rsi(new("/Textures/Structures/Wallmounts/air_monitors.rsi"), "alarmp"),
+            Category = VerbCategory.Tricks,
+            Act = () =>
             {
-                (start,0)
-            };
-            Log.Info($"starting at {start}");
-            while (work.Count > 0)
-            {
-                var part = work.First();
-                var test = part.Item1;
-                work.Remove(part);
-                seen.Add(test);
-                //and we do checking here
-                // SpawnAtPosition(_lizardPlush, test);
+                if (!TryComp<DeviceListComponent>(ent, out var devList))
+                    return;
 
-                var tref = _turf.GetTileRef(test);
-                if (tref == null)
-                    continue; //in space. so we dont continue propogation
-                var stop = _turf.GetEntitiesInTile(test, LookupFlags.StaticSundries)
-                   .Where(x => //Filter for entities on the tile we are checking (stupid walls are just slightly too fat)
-                   {
-                       var tile = _xform.GetGridTilePositionOrDefault(x);
-                       return tile != Vector2i.Zero && tref.Value.GridIndices == tile;
-                   })
-                   .Select(x => {
-                        if (_whitelist.IsWhitelistPass(_linkableWhitelist, x) && TryComp<DeviceNetworkComponent>(x, out var devNetwork))
-                            _deviceList.TryAddDeviceToList(
-                                new(ent, devList),
-                                new(x, devNetwork)
-                            );
-                        Log.Info($"euid: {x} WLPass: {_whitelist.IsWhitelistPass(_linkableWhitelist,x)} DNComp: {HasComp<DeviceNetworkComponent>(x)}");
-                        return _whitelist.IsWhitelistPass(_stoppingWhitelist, x);
-                    })
-                   .Any(x => x);
+                var xform = Transform(ent);
+                var offset = _offset[(int)(
+                   xform.LocalRotation.Degrees / 90 //make it into a mutiple of 90
+                   % 4 //and then normalize to NSEW
+                )];
 
-                if (stop || part.Item2 > MaxDepth)
-                    continue;
+                if (!_xform.TryGetMapOrGridCoordinates(ent, out var coords, xform))
+                    return; //no coords?
 
-                foreach (var direction in _offset.Values)
+                var seen = new HashSet<EntityCoordinates>();
+                var start = coords.Value.Offset(offset);
+                var work = new HashSet<(EntityCoordinates, int)>
                 {
-                    var newWork = test.Offset(direction);
-                    if (!seen.Contains(newWork))
-                        work.Add((newWork, part.Item2 + 2));
+                (start,0)
+                };
+                Log.Info($"starting at {start}");
+                while (work.Count > 0)
+                {
+                    var part = work.First();
+                    var test = part.Item1;
+                    work.Remove(part);
+                    seen.Add(test);
+                    //and we do checking here
+                    // SpawnAtPosition(_lizardPlush, test);
+
+                    var tref = _turf.GetTileRef(test);
+                    if (tref == null)
+                        continue; //in space. so we dont continue propogation
+                    var stop = _turf.GetEntitiesInTile(test, LookupFlags.StaticSundries)
+                       .Where(x => //Filter for entities on the tile we are checking (stupid walls are just slightly too fat)
+                       {
+                           var tile = _xform.GetGridTilePositionOrDefault(x);
+                           return tile != Vector2i.Zero && tref.Value.GridIndices == tile;
+                       })
+                       .Select(x =>
+                       {
+                           if (_whitelist.IsWhitelistPass(_linkableWhitelist, x) && TryComp<DeviceNetworkComponent>(x, out var devNetwork))
+                               _deviceList.TryAddDeviceToList(
+                               new(ent, devList),
+                               new(x, devNetwork)
+                           );
+                           Log.Info($"euid: {x} WLPass: {_whitelist.IsWhitelistPass(_linkableWhitelist, x)} DNComp: {HasComp<DeviceNetworkComponent>(x)}");
+                           return _whitelist.IsWhitelistPass(_stoppingWhitelist, x);
+                       })
+                       .Any(x => x);
+
+                    if (stop || part.Item2 > MaxDepth)
+                        continue;
+
+                    foreach (var direction in _offset.Values)
+                    {
+                        var newWork = test.Offset(direction);
+                        if (!seen.Contains(newWork))
+                            work.Add((newWork, part.Item2 + 2));
+                    }
                 }
+                //xform.LocalRotation
+                //xform.GridUid
             }
-            //xform.LocalRotation
-            //xform.GridUid
-        }
-    });
+        });
+    }
 
 
 }
