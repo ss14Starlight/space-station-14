1d877939f34fcd08bd57fb31c735bd8725115da8
Shin <99152399+wonderfulnewworld@users.noreply.github.com>
2026-09-03 17:29:49 +0000

    After all this time, finally, stains (#5898)
    ## Short description
    Port of Moff Station
    [#1545](https://github.com/moff-station/moff-station-14/pull/1545),
    [#1647](https://github.com/moff-station/moff-station-14/pull/1647),
    [#1695](https://github.com/moff-station/moff-station-14/pull/1695), and
    the washing machine part of
    [#1712](https://github.com/moff-station/moff-station-14/pull/1712).
    
    Adds stains and a washing machine.
    
    Needs https://github.com/ss14Starlight/space-station-14/pull/5893.
    
    ## Why we need to add this
    New visual feedback, good for roleplay and immersion. A highly requested
    feature from Conf.
    
    ## Media (Video/Screenshots)
    
    [Content.Client_BjkUirac71.webm](https://github.com/user-attachments/assets/3d918fd0-170c-4620-87f7-78738dc8ec00)
    
    
    [Content.Client_09l22S1pFv.webm](https://github.com/user-attachments/assets/d76d3953-6e3c-4102-94b3-34009cc2771b)
    
    ## Checks
    - [X] I do not require assistance to complete the PR.
    - [X] Before posting/requesting review of a PR, I have verified that the
    changes work.
    - [X] I have added screenshots/videos of the changes, or this PR does
    not change in-game mechanics.
    - [X] I affirm that my changes are licensed under the [MIT
    License](https://github.com/ss14Starlight/space-station-14/blob/Starlight/LICENSE.TXT)
    and grant permission for use in this repository under its conditions.
    
    **Changelog**
    :cl: wonderfulnewworld, DuckManZach, YaraaraY
    - add: Added a Clothing stain system; your clothes can now get dirty
    with reagents.
    - add: A washing machine with which to wash your clothes. You can also
    clean them by wringing them out manually.
    - add: Added washing machine flatpacks to HoP and CMO's lockers, and the
    Janitor closet.
    
    ---------
    
    Signed-off-by: DuckManZach <144298822+DuckManZach@users.noreply.github.com>
    Co-authored-by: DuckManZach <144298822+DuckManZach@users.noreply.github.com>
    Co-authored-by: MaiaArai <158123176+YaraaraY@users.noreply.github.com>
    Co-authored-by: Centronias <charlie.t.santos@gmail.com>
    Co-authored-by: Nyxilath <colton.malone@gmail.com>
    Co-authored-by: Walker Fowlkes <47339836+walksanatora@users.noreply.github.com>

--- Content.Server/Fluids/EntitySystems/PuddleSystem.cs
+++ Content.Server/Fluids/EntitySystems/PuddleSystem.cs
@@ -14,9 +14,16 @@ using Content.Shared.IdentityManagement;
 using Content.Shared.Maps;
 using Content.Shared.Popups;
 using Content.Shared.Slippery;
+using Content.Shared.Inventory;
+using Content.Shared._Funkystation.Fluids;
+using Content.Shared.Gravity;
+using Content.Shared.Standing;
+using Content.Shared.StepTrigger.Systems;
 using Robust.Shared.Collections;
 using Robust.Shared.Map;
 using Robust.Shared.Map.Components;
+using Robust.Shared.Physics.Components;
+using Robust.Shared.Physics.Events;
 using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
@@ -36,7 +43,6 @@ public sealed partial class PuddleSystem : SharedPuddleSystem
     [Dependency] private SharedSolutionContainerSystem _solutionContainerSystem = default!;
     [Dependency] private SharedTransformSystem _transform = default!;
     [Dependency] private TurfSystem _turf = default!;
-
     private EntityQuery<PuddleComponent> _puddleQuery;
 
     /*
@@ -265,6 +271,14 @@ public sealed partial class PuddleSystem : SharedPuddleSystem
         // Take 15% of the puddle solution
         var splitSol = _solutionContainerSystem.SplitSolution(entity.Comp.Solution.Value, solution.Volume * 0.15f);
         Reactive.DoEntityReaction(args.Slipped, splitSol, ReactionMethod.Touch);
+
+        // Funky - Start - Clothing stains
+        if (splitSol.Volume > 0)
+        {
+            var stainEv = new SpilledOnEvent(entity.Owner, splitSol.Clone());
+            RaiseLocalEvent(args.Slipped, stainEv);
+        }
+        // Funky - End
     }
 
     /// <summary>
@@ -430,6 +444,12 @@ public sealed partial class PuddleSystem : SharedPuddleSystem
 
             targets.Add(owner);
             Reactive.DoEntityReaction(owner, splitSolution, ReactionMethod.Touch);
+
+            // Funky - Start - Clothing stains
+            if (splitSolution.Volume > 0)
+                RaiseLocalEvent(owner, new SpilledOnEvent(entity, splitSolution.Clone()));
+            // Funky - End
+
             Popups.PopupEntity(Loc.GetString("spill-land-spilled-on-other",
                     ("spillable", entity),
                     ("target", Identity.Entity(owner, EntityManager))),
