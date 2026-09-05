946ec16b2025b8c99ad6b4f353e676e64177f19f
Swonkus <168216845+Swonkus@users.noreply.github.com>
2026-09-05 18:03:57 +0200

    Update SalariesPrototype.cs

--- Content.Shared/_Starlight/Economy/SalariesPrototype.cs
+++ Content.Shared/_Starlight/Economy/SalariesPrototype.cs
@@ -16,6 +16,6 @@ public sealed partial class SalariesPrototype : IPrototype
     [DataField]
     public Dictionary<ProtoId<AntagPrototype>, int> Antags = new();
 
-    [DataField] 
+    [DataField]
     public Dictionary<ProtoId<JobPrototype>, string> Sender = new();
 }
