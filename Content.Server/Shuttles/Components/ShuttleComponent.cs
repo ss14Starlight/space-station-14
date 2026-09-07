ab1b8a4be5d5e7a81badabcf16c7bce2d4902b91
Motoko, Firefeathers Pack <117629129+AsunaKusanagi@users.noreply.github.com>
2026-09-07 01:15:33 -0700

    Made BaseMaxLinearVelocity in ShuttleComponent a DataField
    This is from upstream Made BaseMaxLinearVelocity in ShuttleComponent a DataField - #44281

--- Content.Server/Shuttles/Components/ShuttleComponent.cs
+++ Content.Server/Shuttles/Components/ShuttleComponent.cs
@@ -23,7 +23,7 @@
 //         /// <summary>
 //         /// Maximum velocity assuming unupgraded, tier 1 thrusters
 //         /// </summary>
-//         [ViewVariables(VVAccess.ReadWrite)]
+//         [Datafield]
 //         public float BaseMaxLinearVelocity = 20f;
 
 //         public const float MaxAngularVelocity = 4f;
