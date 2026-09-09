69d0f1c8d7374469a129cec24efc5b61687edb26
Citrea_Lingua <citrea.lingua@gmx.net>
2026-09-09 13:53:40 +0200

    dolls now regenerate shell pieces from drinking blood

--- Content.Shared/_Starlight/Dolls/Systems/SnapShellPieceSystem.cs
+++ Content.Shared/_Starlight/Dolls/Systems/SnapShellPieceSystem.cs
@@ -1,11 +1,14 @@
 using System.Linq;
+using Content.Shared._Starlight.Actions.Components;
 using Content.Shared._Starlight.Dolls.Events;
 using Content.Shared._Starlight.Medical.Surgery.Components;
 using Content.Shared._Starlight.Medical.Surgery.Events;
 using Content.Shared.Body.Systems;
 using Content.Shared.Damage.Systems;
+using Content.Shared.EntityEffects;
 using Content.Shared.Hands.EntitySystems;
 using Robust.Shared.Containers;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared._Starlight.Dolls.Systems;
 
@@ -61,4 +64,65 @@ public sealed partial class SnapShellPieceSystem : EntitySystem
 
         ev.Handled = true; //Done!
     }
+}
+
+/// <summary>
+/// Regrows a piece of the shell, with the given prototype
+/// </summary>
+/// <inheritdoc cref="EntityEffectSystem{T,TEffect}"/>
+public sealed partial class RegrowShellEntityEffectSystem : EntityEffectSystem<ShellComponent, RegrowShell>
+{
+    [Dependency] private SharedBodySystem _body = default!;
+    [Dependency] private SharedContainerSystem _container = default!;
+    [Dependency] private IEntityManager _entityManager = default!;
+
+    protected override void Effect(Entity<ShellComponent> entity, ref EntityEffectEvent<RegrowShell> args)
+    {
+        //Find empty shell slots
+        var emptySlots = GetShellContainers(entity.Owner).ToList();
+
+        IEnumerable<BaseContainer> GetShellContainers(EntityUid uid)
+        {
+            var bodyParts = _body.GetBodyChildren(uid).ToList();
+            foreach (var child in bodyParts)
+            {
+                var shellContainer = _container.GetContainer(child.Id, SharedBodySystem.GetOrganContainerId("shell"));
+                if (shellContainer.Count == 0)
+                    yield return shellContainer;
+            }
+        }
+
+        if (emptySlots.Count == 0)
+            return; //No need to regrow anything
+
+        var regrownSlot = emptySlots.Shuffle().First(); //Randomise!
+        var regrownShell = Spawn(args.Effect.ShellProto);
+
+        if(!_container.CanInsert(regrownShell, regrownSlot))
+            QueueDel(regrownShell); //If we can't insert the shell piece for whatever reason, delete it
+        _container.Insert(regrownShell, regrownSlot);
+
+        var part = _body.GetParentPartOrNull(regrownShell);
+        if(part == null)
+            QueueDel(regrownShell); //If insertion somehow didn't put the shell piece into a valid body part, delete it
+        else
+        {
+            var sev = new SurgeryOrganImplantationCompleted(entity.Owner, part.Value, regrownShell);
+            _entityManager.EventBus.RaiseLocalEvent(regrownShell, ref sev);
+        }
+    }
+}
+
+/// <inheritdoc cref="EntityEffect"/>
+public sealed partial class RegrowShell : EntityEffectBase<RegrowShell>
+{
+    /// <summary>
+    /// Which shell prototype are we regrowing?
+    /// </summary>
+    [DataField]
+    public EntProtoId ShellProto = "ShellFragmentDoll";
+
+    public override string EntityEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys, ILocalizationManager loc) // Starlight
+        =>
+            loc.GetString("entity-effect-guidebook-regrow-doll-shell", ("chance", Probability));
 }
