cc70de79a9246975b114579a4b2923446b4866a4
Rinary <rinary.super@gmail.com>
2026-09-10 20:05:35 +0300

    Revert "Footprints and Skid marks (#5899)"
    This reverts commit 68f5dab181ab968e3e0585efabedf8fb3b298631.
    
    # Conflicts:
    #	Content.Server/Fluids/EntitySystems/PuddleSystem.cs
    #	Resources/Prototypes/_Starlight/Entities/Clothing/Shoes/specific.yml

--- Content.Shared/Fluids/SharedAbsorbentSystem.cs
+++ Content.Shared/Fluids/SharedAbsorbentSystem.cs
@@ -1,5 +1,4 @@
 using System.Numerics;
-using Content.Shared._Funkystation.Footprints;
 using Content.Shared.Chemistry.Components;
 using Content.Shared.Chemistry.EntitySystems;
 using Content.Shared.FixedPoint;
@@ -357,18 +356,6 @@ public abstract partial class SharedAbsorbentSystem : EntitySystem
         localPos = userXform.LocalRotation.RotateVec(localPos);
 
         _melee.DoLunge(user, absorbEnt, Angle.Zero, localPos, null);
-
-        // Funky start - Footprints
-        var ev = new FootprintCleanEvent();
-        RaiseLocalEvent(target, ref ev);
-        // Funky end
-
-        // Moff start - Footprint cleaning sounds
-        if (ev.Handled)
-        {
-            _audio.PlayPredicted(absorber.PickupSound, user, user);
-        }
-        // Moff end
 
         return true;
     }
