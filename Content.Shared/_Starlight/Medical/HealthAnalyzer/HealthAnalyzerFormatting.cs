993610d08d4cc0aecdd3ba3cabfac6fdb42b8102
Light <lightidx@gmail.com>
2026-09-11 19:21:11 +0200

    Add vitals and abnormalities hook
    + rotting display

--- Content.Shared/_Starlight/Medical/HealthAnalyzer/HealthAnalyzerFormatting.cs
+++ Content.Shared/_Starlight/Medical/HealthAnalyzer/HealthAnalyzerFormatting.cs
@@ -2,7 +2,7 @@ using Content.Shared.Atmos;
 using Content.Shared.Mobs;
 using Robust.Shared.Utility;
 
-namespace Content.Shared._Starlight.Medical;
+namespace Content.Shared._Starlight.Medical.HealthAnalyzer;
 
 // Shared analyzer formatting for UI and printable reports.
 public static class HealthAnalyzerFormatting
