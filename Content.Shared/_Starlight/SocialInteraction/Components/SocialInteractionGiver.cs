6087b993a41aba2a9f14e5b58e85a9e1a9d81f4e
CawsForConcern <kopczynski.zesty@gmail.com>
2026-09-13 14:54:37 -0700

    and updated properly

--- Content.Shared/_Starlight/SocialInteraction/Components/SocialInteractionGiver.cs
+++ Content.Shared/_Starlight/SocialInteraction/Components/SocialInteractionGiver.cs
@@ -1,7 +1,7 @@
-namespace Content.Shared._Starlight.PhysicalSocialInteraction.Components;
+namespace Content.Shared._Starlight.SocialInteraction.Components;
 
 [RegisterComponent]
-public sealed partial class PhysicalSocialInteractionGiverComponent : Component
+public sealed partial class SocialInteractionGiverComponent : Component
 {
 
 }
