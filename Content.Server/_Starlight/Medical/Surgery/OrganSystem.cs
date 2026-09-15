b2e8f797b10aa000f7233c06b21095da406422aa
RedSpeeds <lars.dormans@live.nl>
2026-09-15 18:20:41 +0200

    Made a few changes to prevent being muted at cloning or transform

--- Content.Server/_Starlight/Medical/Surgery/OrganSystem.cs
+++ Content.Server/_Starlight/Medical/Surgery/OrganSystem.cs
@@ -417,7 +417,8 @@ public sealed partial class OrganSystem : EntitySystem
 
         if (TryComp<VocalComponent>(args.Body, out var vocal) && vocal.EmoteSounds == null)
             _vocal.SetSounds((args.Body, vocal), ent.Comp.Sounds);
-        if (HasComp<AbductorComponent>(args.Body) || !ent.Comp.IsMuted) return;
+        if (ent.Comp.IsMuted) EnsureComp<MutedComponent>(args.Body);
+        if (HasComp<AbductorComponent>(args.Body)) return;
         RemComp<MutedComponent>(args.Body);
     }
 
@@ -442,7 +443,7 @@ public sealed partial class OrganSystem : EntitySystem
             _vocal.SetSounds((args.Body, vocal), null);
 
         ent.Comp.IsMuted = HasComp<MutedComponent>(args.Body);
-        AddComp<MutedComponent>(args.Body);
+        EnsureComp<MutedComponent>(args.Body);
     }
 
     //
