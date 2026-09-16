99440a2f726f2653b92010d1f792e569b66f6c10
wonderfulnewworld <shinsekaihibiki@gmail.com>
2026-09-16 13:53:57 -0700

    ok ai god

--- Content.Shared/Chemistry/Reagent/ReagentPrototype.Starlight.cs
+++ Content.Shared/Chemistry/Reagent/ReagentPrototype.Starlight.cs
@@ -18,7 +18,7 @@ namespace Content.Shared.Chemistry.Reagent
         public bool SelfOxidizing;
 
         /// <summary>
-        /// Minimum quantity required for this reagent's movement effects to apply in a puddle.
+        /// Minimum quantity required for this reagent's viscosity to apply in a puddle.
         /// </summary>
         [DataField]
         public FixedPoint2 ViscosityMin = FixedPoint2.Zero;
