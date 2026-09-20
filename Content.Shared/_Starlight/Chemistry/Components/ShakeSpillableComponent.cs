c53d578bdaba1a731f130903adc1487ba09744c7
this guy <128647311+this-guy-git@users.noreply.github.com>
2026-09-20 13:25:54 -0700

    Update Content.Shared/_Starlight/Chemistry/Components/ShakeSpillableComponent.cs
    Co-authored-by: Rinary <rinary.super@gmail.com>

--- Content.Shared/_Starlight/Chemistry/Components/ShakeSpillableComponent.cs
+++ Content.Shared/_Starlight/Chemistry/Components/ShakeSpillableComponent.cs
@@ -2,7 +2,6 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared._Starlight.Chemistry.Components;
 
-// Starlight-start: Bottles spill without cap when shaken
 [RegisterComponent, NetworkedComponent]
 public sealed partial class ShakeSpillableComponent : Component
 {
