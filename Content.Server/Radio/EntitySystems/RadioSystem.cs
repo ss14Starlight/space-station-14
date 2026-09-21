4a0255541926e48694b448f8ad24031dd64a3156
depressedtimbit <84045706+depressedtimbit@users.noreply.github.com>
2026-09-21 19:22:38 -0400

    Partialized a tiny bit of radio

--- Content.Server/Radio/EntitySystems/RadioSystem.cs
+++ Content.Server/Radio/EntitySystems/RadioSystem.cs
@@ -29,7 +29,6 @@ using Content.Shared._Starlight.Language.Components;
 using Content.Shared.Ghost;
 using Content.Server._Starlight.TextToSpeech;
 using Content.Shared._Starlight.Clothing;
-using Content.Shared.Mobs.Systems;
 using Content.Shared.Popups;
 
 namespace Content.Server.Radio.EntitySystems;
@@ -48,9 +47,6 @@ public sealed partial class RadioSystem : EntitySystem
     [Dependency] private ChatSystem _chat = default!;
     [Dependency] private AccessReaderSystem _accessReader = default!;
     [Dependency] private RadioChimeSystem _chime = default!; //🌟Starlight🌟
-    [Dependency] private MobStateSystem _mobState = default!; //🌟Starlight🌟
-    [Dependency] private SharedPopupSystem _popup = default!; //🌟Starlight🌟
-    [Dependency] private ISharedPlayerManager _playerManager = default!; //🌟Starlight🌟
     [Dependency] private LanguageSystem _language = default!; // Starlight
 
     // set used to prevent radio feedback loops.
@@ -134,28 +130,11 @@ public sealed partial class RadioSystem : EntitySystem
         bool escapeMarkup = true,
         HeadsetLoudModeComponent? loudComp = null) // Starlight
     {
-        // Starlight - start
-        if (channel.AutoTranslate is not null)
-            language = _language.GetLanguagePrototype(channel.AutoTranslate.Value);
-
-        if (language == null)
-            language = _language.GetLanguage(messageSource);
-
-        if ((!language.Speech.AllowRadio && language.Speech.RadioChannel is not null && language.Speech.RadioChannel != channel)
-            || (!language.Speech.AllowRadio && language.Speech.RadioChannel is null))
-            return;
-
-        if (_mobState.IsSoftCritical(messageSource))
-        {
-            if (_playerManager.TryGetSessionByEntity(messageSource, out var session))
-                _popup.PopupEntity(Loc.GetString("radio-failed-to-send-soft-critical"), messageSource, session, PopupType.Medium);
-            return;
-        }
-        // Starlight - End
-
-        // TODO if radios ever garble / modify messages, feedback-prevention needs to be handled better than this.
-        if (!_messages.Add(message.Text)) // Starlight
-            return;
+        // Starlight Start
+        if (SendRadioMessageSL(messageSource, channel, ref language)) return;
+        if (language is null) return;
+        if (!_messages.Add(message.Text)) return;  // TODO if radios ever garble / modify messages, feedback-prevention needs to be handled better than this.
+        // Starlight end
 
         var meta = MetaData(messageSource);
         var entityName = meta?.EntityName ?? string.Empty;
