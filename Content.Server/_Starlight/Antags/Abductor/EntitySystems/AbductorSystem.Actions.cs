1f7f35da59ec92d6e046d1c0c6f63b2d1f311cee
Adonalsium <Scarecrow_0909@proton.me>
2026-09-21 08:16:56 -0500

    fixed abductor bug

--- Content.Server/_Starlight/Antags/Abductor/EntitySystems/AbductorSystem.Actions.cs
+++ Content.Server/_Starlight/Antags/Abductor/EntitySystems/AbductorSystem.Actions.cs
@@ -77,7 +77,7 @@ public sealed partial class AbductorSystem : SharedAbductorSystem
 
     private void OnReturn(AbductorReturnToShipEvent ev)
     {
-        // Check if abductor is stunned, cuffed, or dead- if so, cancel the return
+        // Check if abductor is stunned or cuffed- if so, cancel the return
         if (HasComp<StunnedComponent>(ev.Performer))
         {
             _popup.PopupEntity(Loc.GetString("abductor-return-stunned"), ev.Performer, ev.Performer);
