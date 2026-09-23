4095f8b8e9bd8428f40fe2998aeeefb16b100507
Balt <59123926+balt-dev@users.noreply.github.com>
2026-09-23 18:37:55 -0500

    Update Content.Server/Radio/EntitySystems/RadioDeviceSystem.cs
    Co-authored-by: Rinary <rinary.super@gmail.com>

--- Content.Server/Radio/EntitySystems/RadioDeviceSystem.cs
+++ Content.Server/Radio/EntitySystems/RadioDeviceSystem.cs
@@ -275,7 +275,7 @@ public sealed partial class RadioDeviceSystem : SharedRadioDeviceSystem
 
     private void OnToggleIntercomSpeaker(Entity<IntercomComponent> ent, ref ToggleIntercomSpeakerMessage args)
     {
-        if (HasComp<ItemToggleComponent>(ent)) return;
+        if (HasComp<ItemToggleComponent>(ent)) return; // Starlight-edit
 
         if (ent.Comp.RequiresPower && !this.IsPowered(ent, EntityManager))
             return;
