f1be85015f96650826f0b9da64cd5be66dfbddee
VBlankFF <vblankff@gmail.com>
2026-09-23 09:16:59 -0400

    Pass the user's job as a parameter to the deathrattle message

--- Content.Server/Trigger/Systems/RattleOnTriggerSystem.cs
+++ Content.Server/Trigger/Systems/RattleOnTriggerSystem.cs
@@ -5,7 +5,10 @@ using Content.Shared.Mobs.Components;
 using Content.Shared.Trigger;
 using Content.Shared.Trigger.Components.Effects;
 using Content.Server.Chat.Systems; // Starlight
+using Content.Server.StationRecords.Systems; // Starlight
+using Content.Shared.StationRecords; // Starlight
 using Content.Shared.Database; // Starlight
+using Content.Server.Station.Systems; // Starlight
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
@@ -18,6 +21,8 @@ public sealed partial class RattleOnTriggerSystem : EntitySystem
     [Dependency] private NavMapSystem _navMap = default!;
     [Dependency] private ChatSystem _chat = default!; // Starlight
     [Dependency] private IAdminLogManager _adminLogger = default!; // Starlight
+    [Dependency] private StationRecordsSystem _recordsSystem = default!; // Starlight
+    [Dependency] private StationSystem _station = default!;
 
     public override void Initialize()
     {
@@ -51,7 +56,23 @@ public sealed partial class RattleOnTriggerSystem : EntitySystem
         var posText = FormattedMessage.RemoveMarkupOrThrow(_navMap.GetNearestBeaconString(target.Value));
         var nameText = FormattedMessage.RemoveMarkupOrThrow(MetaData(target.Value).EntityName); // Starlight: Sanitize name
 
-        var message = Loc.GetString(messageId, ("user", nameText), ("position", posText)); // Starlight: Sanitized name
+        #region Starlight
+        // Gets the job title of the user in the manifest
+        var station = _station.GetOwningStation(ent);
+        var jobName = Loc.GetString("rattle-on-trigger-job-unknown");
+        if (TryComp<StationRecordsComponent>(station, out var stationRecords))
+        {
+            var recordId = _recordsSystem.GetRecordByName(station.Value, MetaData(target.Value).EntityName);
+            if (recordId != null)
+            {
+                var key = new StationRecordKey(recordId.Value, station.Value);
+                if (_recordsSystem.TryGetRecord<GeneralStationRecord>(key, out var entry, stationRecords))
+                    jobName = entry.JobTitle;
+            }
+        }
+        #endregion
+
+        var message = Loc.GetString(messageId, ("user", nameText), ("job", jobName), ("position", posText)); // Starlight: Sanitized name, added job name to parameters
         #region Starlight
         //For global announcements, ie, DAGD nuke codes, so they can't just sabotage comms. Could also use this to announce when someone important is dead.
         if (ent.Comp.Global)
