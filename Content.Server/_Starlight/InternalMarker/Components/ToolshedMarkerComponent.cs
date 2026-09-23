675d468e3c53a1c520279dc1ee482fb7dd826b0a
Pickra <1034958+Unrealomega@users.noreply.github.com>
2026-09-23 10:04:18 -0400

    Added ToolshedMarker component

---
+++ Content.Server/_Starlight/InternalMarker/Components/ToolshedMarkerComponent.cs
@@ -0,0 +1,21 @@
+﻿namespace Content.Server._Starlight.InternalMarker.Components;
+
+/// <summary>
+///     The intent of this component is to track objects and it's data for script purposes.
+///     Because Toolshed is server-side only, this only has to exist here and doesn't need to be networked.
+/// </summary>
+[RegisterComponent]
+public sealed partial class ToolshedMarkerComponent : Component
+{
+    /// <summary>
+    ///     Named ID of our marker component
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public String Id = "";
+
+    /// <summary>
+    ///     The data it will be storing, if any
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public Dictionary<String, String> data = [];
+}
