cd6c523126d47222f3780f90cb4aa6f8e43997ce
Sparlight <twiksparlight@gmail.com>
2026-09-23 09:52:05 -0600

    remove unused using

--- Content.Shared/_Starlight/Scent/Components/SmellerComponent.cs
+++ Content.Shared/_Starlight/Scent/Components/SmellerComponent.cs
@@ -2,7 +2,6 @@ using Content.Shared._Starlight.Scent.Systems;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
 namespace Content.Shared._Starlight.Scent.Components;
 
