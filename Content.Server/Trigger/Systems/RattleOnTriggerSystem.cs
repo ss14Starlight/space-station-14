4ad8fc3f16e4d91d5dbabf85baf0f9178655b4b9
VBlankFF <vblankff@gmail.com>
2026-09-24 19:20:42 -0400

    Update Content.Server/Trigger/Systems/RattleOnTriggerSystem.cs
    Co-authored-by: Rinary <rinary.super@gmail.com>

--- Content.Server/Trigger/Systems/RattleOnTriggerSystem.cs
+++ Content.Server/Trigger/Systems/RattleOnTriggerSystem.cs
@@ -105,10 +105,8 @@ public sealed partial class RattleOnTriggerSystem : EntitySystem
             }
         }
         jobName = FormattedMessage.RemoveMarkupOrThrow(jobName);
-        // Starlight-end
 
-        var message = Loc.GetString(messageId, ("user", nameText), ("job", jobName), ("position", posText)); // Starlight: Sanitized name, added job name to parameters
-        // Starlight-start
+        var message = Loc.GetString(messageId, ("user", nameText), ("job", jobName), ("position", posText));
         //For global announcements, ie, DAGD nuke codes, so they can't just sabotage comms. Could also use this to announce when someone important is dead.
         if (ent.Comp.Global)
         {
