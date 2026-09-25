d2032ecfb2dbada5c99a1bacb257cc59d341eaae
Rhapsody <rhapsodysmail@gmail.com>
2026-09-25 12:50:30 -0400

    Additional triggers for citrus
    And some punctuation fixes for some meals in meals.yml because the lack of punctuation on these was odd and always bothered me.

---
+++ Content.Shared/_Starlight/XenoArch/CitrusComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Shared._Starlight.XenoArch;
+
+/// <summary>
+/// Marker for items that should activate the "citrus" triggers on artifacts by proximity.
+/// </summary>
+[RegisterComponent]
+public sealed partial class CitrusComponent : Component;
--- Content.Shared/_Starlight/XenoArch/FloppyDiskComponent.cs
+++ Content.Shared/_Starlight/XenoArch/FloppyDiskComponent.cs
@@ -1,7 +1,7 @@
 namespace Content.Shared._Starlight.XenoArch;
 
 /// <summary>
-/// Marker for items that should activate the "floppy storage" triggers on artifacts.
+/// Marker for items that should activate the "floppy storage" triggers on artifacts by proximity.
 /// </summary>
 [RegisterComponent]
 public sealed partial class FloppyDiskComponent : Component;
